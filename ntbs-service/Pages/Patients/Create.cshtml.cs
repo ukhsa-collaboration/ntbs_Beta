fb61e97b55052c511dbef2d0fec0d0f3a694db66
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-08-23 16:45:27 +0100

    NTBS-87 Merge create & edit, remove unused pages

--- ntbs-service/Pages/Patients/Create.cshtml.cs
+++ ntbs-service/Pages/Patients/Create.cshtml.cs
@@ -3,96 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ntbs_service.DataAccess;
 using ntbs_service.Models;
-using ntbs_service.Helpers;
-using ntbs_service.Services;
 
 namespace ntbs_service.Pages_Patients
 {
     public class CreateModel : PageModel
     {
-        private readonly IPatientService service;
-        private readonly NtbsContext _context;
         private readonly IPatientRepository _repository;
 
-        public SelectList Ethnicities { get; set;}
-        public SelectList Countries { get; set; }
-        public List<Sex> Sexes { get; set; }
-
-        public CreateModel(IPatientService service, NtbsContext context, IPatientRepository repository)
+        public CreateModel(IPatientRepository repository)
         {
-            this.service = service;
-            _context = context;
             _repository = repository;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
-            Ethnicities = new SelectList(_context.GetAllEthnicitiesAsync().Result, nameof(Ethnicity.EthnicityId), nameof(Ethnicity.Label));
-            Countries = new SelectList(_context.GetAllCountriesAsync().Result, nameof(Country.CountryId), nameof(Country.Name));
-            Sexes = _context.GetAllSexesAsync().Result.ToList();
-            return Page();
-        }
-
-        [BindProperty]
-        public Patient Patient { get; set; }
-        [BindProperty]
-        public FormattedDate FormattedDob { get; set; }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            SetAndValidateBirthDate();
-
-            if (!ModelState.IsValid)
-            {
-                return OnGet();
-            }
-
-            service.UpdateUkBorn(Patient);
+            var Patient = new Patient();
             await _repository.AddPatientAsync(Patient);
-
-            return RedirectToPage("./Index");
+            return RedirectToPage("./Edit", new {id = Patient.PatientId });
         }
 
-        public ContentResult OnPostValidateProperty(string key, string value)
-        {
-            Patient.GetType().GetProperty(key).SetValue(Patient, value);
-            if (TryValidateModel(Patient)) {
-                return Content("");
-            } else {
-                var model = ModelState[key];
-                return Content(ModelState[key].Errors[0].ErrorMessage);
-            }
-        }
-
-
-        public bool IsValid(string key)
-        {
-            return ModelState[key] == null ? true : ModelState[key].Errors.Count == 0;
-        }
-
-        private void SetAndValidateBirthDate()
-        {
-            if (FormattedDob.IsEmpty()) {
-                return;
-            }
-
-            var patientKey = "Patient.Dob";
-            DateTime? convertedDob;
-
-            if (FormattedDob.TryConvertToDateTime(out convertedDob)) {
-                Patient.Dob = convertedDob;
-                TryValidateModel(Patient, "Patient");
-            }
-            else
-            {
-                ModelState.AddModelError(patientKey, "Please enter a valid date");
-                return;
-            }
-        }
     }
 }
--- ntbs-service/Pages/Patients/Edit.cshtml.cs
+++ ntbs-service/Pages/Patients/Edit.cshtml.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using ntbs_service.DataAccess;
 using ntbs_service.Models;
+using ntbs_service.Helpers;
+using ntbs_service.Services;
 
 namespace ntbs_service.Pages_Patients
 {
     public class EditModel : PageModel
     {
+        private readonly IPatientService service;
         private readonly NtbsContext _context;
         private readonly IPatientRepository _repository;
 
-        public EditModel(NtbsContext context, IPatientRepository repository)
+        public SelectList Ethnicities { get; set;}
+        public SelectList Countries { get; set; }
+        public List<Sex> Sexes { get; set; }
+
+        public EditModel(IPatientService service, NtbsContext context, IPatientRepository repository)
         {
+            this.service = service;
             _context = context;
             _repository = repository;
         }
-
-        [BindProperty]
-        public Patient Patient { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -38,35 +43,68 @@ namespace ntbs_service.Pages_Patients
             {
                 return NotFound();
             }
-            // ViewData["RegionId"] = new SelectList(_context.Region, "RegionId", "Label");
-            ViewData["SexId"] = new SelectList(_context.Sex, "SexId", "Label");
+
+            Ethnicities = new SelectList(_context.GetAllEthnicitiesAsync().Result, nameof(Ethnicity.EthnicityId), nameof(Ethnicity.Label));
+            Countries = new SelectList(_context.GetAllCountriesAsync().Result, nameof(Country.CountryId), nameof(Country.Name));
+            Sexes = _context.GetAllSexesAsync().Result.ToList();
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        [BindProperty]
+        public Patient Patient { get; set; }
+        [BindProperty]
+        public FormattedDate FormattedDob { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
+            SetAndValidateBirthDate();
+
             if (!ModelState.IsValid)
             {
-                return Page();
+                return await OnGetAsync(id);
             }
 
-            try
-            {
-                await _repository.UpdatePatientAsync(Patient);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!_repository.PatientExists(Patient.PatientId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            service.UpdateUkBorn(Patient);
+            await _repository.UpdatePatientAsync(Patient);
 
             return RedirectToPage("./Index");
+        }
+
+        public ContentResult OnPostValidateProperty(string key, string value)
+        {
+            Patient.GetType().GetProperty(key).SetValue(Patient, value);
+            if (TryValidateModel(Patient)) {
+                return Content("");
+            } else {
+                var model = ModelState[key];
+                return Content(ModelState[key].Errors[0].ErrorMessage);
+            }
+        }
+
+
+        public bool IsValid(string key)
+        {
+            return ModelState[key] == null ? true : ModelState[key].Errors.Count == 0;
+        }
+
+        private void SetAndValidateBirthDate()
+        {
+            if (FormattedDob.IsEmpty()) {
+                return;
+            }
+
+            var patientKey = "Patient.Dob";
+            DateTime? convertedDob;
+
+            if (FormattedDob.TryConvertToDateTime(out convertedDob)) {
+                Patient.Dob = convertedDob;
+                TryValidateModel(Patient, "Patient");
+            }
+            else
+            {
+                ModelState.AddModelError(patientKey, "Please enter a valid date");
+                return;
+            }
         }
     }
 }
