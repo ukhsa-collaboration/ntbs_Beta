d68831c986d643ef502e53093bf13092da039553
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-08-27 12:30:40 +0100

    NTBS-87 Add govuk's conditional radios

--- ntbs-service/wwwroot/source/app.ts
+++ ntbs-service/wwwroot/source/app.ts
@@ -1,3 +1,6 @@
+import { initAll as govUkJsInitAll } from 'govuk-frontend'
+// Govuk css - needed for things like conditionally revealed radios sections
+import 'govuk-frontend/govuk/all.scss';
 import 'nhsuk-frontend/packages/nhsuk'
 import 'nhsuk-frontend/packages/nhsuk.scss'
 import '../css/site.css'
@@ -5,6 +8,9 @@ import Vue from 'vue';
 import { TestButton } from './Components/TestButton';
 import { ValidateInput } from './Components/ValidateInput';
 import { ValidateDate } from './Components/ValidateDate';
+
+// Vue needs to be the firs thing to load!
+// Otherwise, it replaces the templates of its components with fresh content, potentially overwriting changes from other scripts!
 
 // register Vue components
 Vue.component('test-button', TestButton);
@@ -17,3 +23,5 @@ new Vue({
 
 // Enables ASP hot relaod
 module.hot.accept()
+
+govUkJsInitAll()
