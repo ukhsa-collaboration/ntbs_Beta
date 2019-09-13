5a908b1d3d6eedca1186aba81607a580a5ce1e34
Christian McCaffery <Christian.McCaffery@softwire.com>
2019-09-13 08:51:44 +0100

    NTBS-184 create skeleton pages for overview page

---
+++ ntbs-service/Pages/Notifications/Overview.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ntbs_service.Helpers;
+using ntbs_service.Models;
+using ntbs_service.Pages;
+using ntbs_service.Services;
+
+namespace ntbs_service.Pages_Notifications
+{
+    public class OverviewModel : ValidationModel
+    {
+        private readonly INotificationService service;
+        private readonly NtbsContext context;
+
+        public OverviewModel(INotificationService service, NtbsContext context)
+        {
+            this.service = service;
+            this.context = context;
+        }
+
+        [BindProperty]
+        public Notification Notification { get; set; }
+        [BindProperty]
+        public int NotificationId { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            Notification = await service.GetNotificationAsync(id);
+            if (Notification == null)
+            {
+                return NotFound();
+            }
+
+            NotificationId = Notification.NotificationId;
+
+            return Page();
+        }
+    }
+}
