738ce77afffb6c901ec96b7146f9de5994c4c758
Christian McCaffery <Christian.McCaffery@softwire.com>
2019-10-07 12:18:37 +0100

    NTBS-347 start work on PartialDate class

--- ntbs-service/Models/PartialDate.cs
+++ ntbs-service/Models/PartialDate.cs
@@ -6,9 +6,51 @@ namespace ntbs_service.Models
     [NotMapped]
     public class PartialDate
     {
-        public DateTime Date { get; set; }
+        public string Day {get; set; }
+        public string Month {get; set; }
+        public string Year  {get; set; }
         public DateType Type { get; set; }
 
+        public bool IsEmpty()
+        {
+            return string.IsNullOrEmpty(Year) && string.IsNullOrEmpty(Month) && string.IsNullOrEmpty(Day);
+        }
+
+        public bool TryConvertToDateTime(out DateTime? dateTime)
+        {
+            dateTime = null;
+
+            int parsedDay;
+            int parsedMonth;
+            int parsedYear;
+
+            if(Type == DateType.Year) {
+                int.TryParse(Year, out parsedYear);
+                parsedMonth = 0;
+                parsedDay = 1;
+            } 
+            if(Type == DateType.MonthYear) {
+
+            }
+            if(Type == DateType.DayMonthYear) {
+
+            }
+
+            if (int.TryParse(Day, out parsedDay) && int.TryParse(Month, out parsedMonth) 
+                && int.TryParse(Year, out parsedYear))
+            {
+                try
+                {
+                    dateTime = new DateTime(parsedYear, parsedMonth, parsedDay);
+                    return true;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
     }
 
     public enum DateType
