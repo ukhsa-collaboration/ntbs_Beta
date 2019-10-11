99ce7a23be830a42d7e405d05c9b49ffd14f464c
Conor Sheehan <Conor.Sheehan@softwire.com>
2019-10-11 16:49:18 +0100

    NTBS-206: Add Address field to Notification entity, with some sneaky code quality improvements

--- ntbs-service/Models/Validations/ValidationHelper.cs
+++ ntbs-service/Models/Validations/ValidationHelper.cs
@@ -17,6 +17,7 @@ namespace ntbs_service.Models.Validations
         }
 
         public const string StandardStringFormat = "Can only contain letters and ' - . ,";
+        public const string StringWithNumbersAndForwardSlashFormat = "Can only contain letters, numbers and ' - . , /";
         public const string NhsNumberFormat = "NHS Number can only contain digits 0-9";
         public const string NumberFormat = "Can only contain digits 0-9";
         public const string NhsNumberLength = "NHS Number needs to be 10 digits long";
@@ -68,5 +69,6 @@ namespace ntbs_service.Models.Validations
     public static class ValidationRegexes
     {
         public const string CharacterValidation = @"[a-zA-Z \-,.']+";
+        public const string CharacterValidationWithNumbersForwardSlashAndNewLine = @"[0-9a-zA-Z \/\-,.'\n\r]+";
     }
 }
--- ntbs-service/wwwroot/source/Components/ValidateInput.ts
+++ ntbs-service/wwwroot/source/Components/ValidateInput.ts
@@ -1,52 +1,57 @@
-import Vue from 'vue';
-import { getHeaders, getValidationPath } from '../helpers';
-import axios from 'axios';
+import Vue from "vue";
+import { getHeaders, getValidationPath } from "../helpers";
+import axios from "axios";
 
 const ValidateInput = Vue.extend({
-    props: ['model', 'property', 'shouldvalidatefull'],
+    props: ["model", "property", "shouldvalidatefull"],
     methods: {
-      validate: function (event: FocusEvent) {
-          // Our onBlur validate events happen on input fields
-          const inputField = event.target as HTMLInputElement
-          const newValue = inputField.value;
+        validate: function (event: FocusEvent) {
+            // Our onBlur validate events happen on input fields
+            const inputField = event.target as HTMLInputElement;
+            const newValue = inputField.value;
 
-        let requestConfig = {
-          url: `${getValidationPath(this.$props.model)}Property`,
-          headers: getHeaders(),
-          params: {
-            "value": newValue,
-            "shouldValidateFull": this.$props.shouldvalidatefull,
-            "key": this.$props.property
-          }
-        }
-        axios.request(requestConfig)
-        .then((response: any) => {
-            var errorMessage = response.data;
-            var hasError = errorMessage != '';
-            if (hasError) {
-              this.$el.classList.add('nhsuk-form-group--error');
-              if (this.$refs["inputField"]) {
-                this.$refs["inputField"].classList.add('nhsuk-input--error')
-              }
-              if (this.$refs["selectField"]) {
-                this.$refs["selectField"].classList.add('nhsuk-select--error')
-              }
-            } else {
-              this.$el.classList.remove('nhsuk-form-group--error')
-              if (this.$refs["inputField"]) {
-                this.$refs["inputField"].classList.remove('nhsuk-input--error')
-              }
-              if (this.$refs["selectField"]) {
-                this.$refs["selectField"].classList.remove('nhsuk-select--error')
-              }
+            let requestConfig = {
+                url: `${getValidationPath(this.$props.model)}Property`,
+                headers: getHeaders(),
+                params: {
+                    "value": newValue,
+                    "shouldValidateFull": this.$props.shouldvalidatefull,
+                    "key": this.$props.property
+                }
             }
-            this.$refs["errorField"].textContent = errorMessage;
-          })
-        .catch((error: any) => {
-            console.log(error.response)
-        });
+            axios.request(requestConfig)
+                .then((response: any) => {
+                    var errorMessage = response.data;
+                    if (errorMessage) {
+                        this.$el.classList.add("nhsuk-form-group--error");
+                        if (this.$refs["inputField"]) {
+                            this.$refs["inputField"].classList.add("nhsuk-input--error");
+                        }
+                        if (this.$refs["selectField"]) {
+                            this.$refs["selectField"].classList.add("nhsuk-select--error");
+                        }
+                        if (this.$refs["textareaField"]) {
+                            this.$refs["textareaField"].classList.add("nhsuk-textarea--error");
+                        }
+                    } else {
+                        this.$el.classList.remove("nhsuk-form-group--error");
+                        if (this.$refs["inputField"]) {
+                            this.$refs["inputField"].classList.remove("nhsuk-input--error");
+                        }
+                        if (this.$refs["selectField"]) {
+                            this.$refs["selectField"].classList.remove("nhsuk-select--error");
+                        }
+                        if (this.$refs["textareaField"]) {
+                            this.$refs["textareaField"].classList.remove("nhsuk-textarea--error");
+                        }
+                    }
+                    this.$refs["errorField"].textContent = errorMessage;
+                })
+                .catch((error: any) => {
+                    console.log(error.response);
+                });
+        }
     }
-  }
 });
 
 export {
