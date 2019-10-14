47f3bca5dd8a460a5eec38ec6e8022d6845bff2a
Christian McCaffery <Christian.McCaffery@softwire.com>
2019-10-14 14:50:24 +0100

    NTBS-188 move getAttribute function to enumExtensions in ExtensionMethods

--- ntbs-service/Helpers/ExtensionMethods.cs
+++ ntbs-service/Helpers/ExtensionMethods.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using ntbs_service.Models;
 
 namespace ntbs_service.Helpers
@@ -8,6 +10,18 @@ namespace ntbs_service.Helpers
         public static FormattedDate ConvertToFormattedDate(this DateTime? dateTime) {
 
             return new FormattedDate() { Day = dateTime?.Day.ToString(), Month = dateTime?.Month.ToString(), Year = dateTime?.Year.ToString() };
+        }
+    }
+
+    public static class EnumExtension
+    {
+        public static TAttribute GetAttribute<TAttribute>(Enum enumValue) 
+            where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
         }
     }
 }
