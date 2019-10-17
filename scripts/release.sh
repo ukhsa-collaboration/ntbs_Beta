a470120f7831efdc458431349563a53491ec8e63
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-10-17 12:30:34 +0100

    NTBS-459 Refactor release health loop

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,20 +13,20 @@ echo "Setting image to build $build"
 # This sets the image to current build. Should be the same as "latest", but triggers pull of the image.
 kubectl set image deployment/ntbs-$env ntbs-$env=ntbscontainerregistry.azurecr.io/ntbs-service:$build
 
-notReady=1
 attempts=0
+maxAttempts=20
 
-while [[ $notReady -ne 0 ]]; do
+while [[ $attempts -lt $maxAttempts ]]; do
   ((attempts=attempts+1))
   sleep .5
-  notReady=$(kubectl get deployment ntbs-int -o jsonpath="{.status.unavailableReplicas}")
-  echo "Attempt $attempts - unavailable replicas found: ${notReady}"
+  unavailableReplicas=$(kubectl get deployment ntbs-int -o jsonpath="{.status.unavailableReplicas}")
+  echo "Attempt $attempts/$maxAttempts - unavailable replicas found: ${notReady}"
   
-  if [[ $attempts -eq 20 ]]; then break; fi
+  if [[ $unavailableReplicas -eq 0 ]]; then
+    echo "Deployment done"
+    exit 0
+  fi
 done
 
-if [[ $notReady -ne 0 ]]; then
-  exit 1;
-fi
-
-echo "Deployment done"
+# Max number of attempts exceeded
+exit 1
