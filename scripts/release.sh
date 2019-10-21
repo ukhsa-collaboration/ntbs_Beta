c70a84d86d67568f6cfc026f4db598b68c0c58e1
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-10-21 12:49:00 +0100

    Release process improvements
    - Add hash to the commit in build id
    - Record deployment command in deployments' metadata

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,11 +7,11 @@ build="$2"
 echo "Deploying to int"
 
 echo "Applying resource definion $env.yml"
-kubectl apply -f ./ntbs-service/deployments/$env.yml
+kubectl apply -f ./ntbs-service/deployments/$env.yml --record=true
 
 echo "Setting image to build $build"
 # This sets the image to current build. Should be the same as "latest", but triggers pull of the image.
-kubectl set image deployment/ntbs-$env ntbs-$env=ntbscontainerregistry.azurecr.io/ntbs-service:$build
+kubectl set image deployment/ntbs-$env ntbs-$env=ntbscontainerregistry.azurecr.io/ntbs-service:$build --record=true
 
 attempts=0
 maxAttempts=20
