87f6758a742e937fea97f918a10d919cd2dd2a1e
Christian McCaffery <Christian.McCaffery@softwire.com>
2019-10-24 16:05:51 +0100

    NTBS-527 add new db fields

---
+++ ntbs-service/Migrations/20191024145734_AddDOTAndEnhancedCaseManagement.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class AddDOTAndEnhancedCaseManagement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDOT",
+                table: "ClinicalDetails",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isEnhancedCaseManagement",
+                table: "ClinicalDetails",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDOT",
+                table: "ClinicalDetails");
+
+            migrationBuilder.DropColumn(
+                name: "isEnhancedCaseManagement",
+                table: "ClinicalDetails");
+        }
+    }
+}
