747fe08d0b2bf0e5ee549c9a54513304439566b3
Christian McCaffery <Christian.McCaffery@softwire.com>
2019-10-30 17:12:01 +0000

    NTBS-527 add migration for changed fields

---
+++ ntbs-service/Migrations/20191030171051_ChangeDotAndEnhancedCaseManagementToStatus.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class ChangeDotAndEnhancedCaseManagementToStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDOT",
+                table: "ClinicalDetails");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnhancedCaseManagement",
+                table: "ClinicalDetails");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DotStatus",
+                table: "ClinicalDetails",
+                maxLength: 30,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EnhancedCaseManagementStatus",
+                table: "ClinicalDetails",
+                maxLength: 30,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DotStatus",
+                table: "ClinicalDetails");
+
+            migrationBuilder.DropColumn(
+                name: "EnhancedCaseManagementStatus",
+                table: "ClinicalDetails");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDOT",
+                table: "ClinicalDetails",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnhancedCaseManagement",
+                table: "ClinicalDetails",
+                nullable: true);
+        }
+    }
+}
