b03b4f26948c5f43b0a760011d5bec0d4c3dd220
Conor Sheehan <Conor.Sheehan@softwire.com>
2019-11-07 13:41:19 +0000

    NTBS-496: Address remaining unconditional error warnings

--- ntbs-service/wwwroot/source/Components/ValidateDate.ts
+++ ntbs-service/wwwroot/source/Components/ValidateDate.ts
@@ -1,16 +1,16 @@
-import Vue from 'vue';
-import { getHeaders, getValidationPath as getValidationPath, FormattedDate, convertFormattedDateToDate } from '../helpers';
-const axios = require('axios');
+import Vue from "vue";
+import { getHeaders, getValidationPath as getValidationPath, FormattedDate, convertFormattedDateToDate } from "../helpers";
+const axios = require("axios");
 
 const ValidateDate = Vue.extend({
-    props: ['model', 'property', 'notification_id', 'name', 'rank'],
+    props: ["model", "property", "notification_id", "name", "rank"],
     mounted: function () {
         if (this.rank) {
             // v-model binds to the input event, so this gets picked up by the containing DateComparison component, if present
             // See https://vuejs.org/v2/guide/components.html#Using-v-model-on-Components
-            var formattedDate: FormattedDate = this.getFormattedDate();
+            const formattedDate: FormattedDate = this.getFormattedDate();
             if (formattedDate) {
-                this.$emit('input', convertFormattedDateToDate(formattedDate));
+                this.$emit("input", convertFormattedDateToDate(formattedDate));
             }
         }
     },
@@ -19,7 +19,7 @@ const ValidateDate = Vue.extend({
             var date: FormattedDate = this.getFormattedDate();
             if (!date) {
                 if (this.rank) {
-                    this.$emit('input', null);
+                    this.$emit("input", null);
                     this.$parent.datechanged(this.rank);
                 }
                 return;
@@ -42,35 +42,34 @@ const ValidateDate = Vue.extend({
                 .then((response: any) => {
                     console.log(response);
                     var errorMessage = response.data;
-                    var hasError = errorMessage != '';
 
                     this.$refs["errorField"].textContent = errorMessage;
-                    if (hasError) {
-                        this.$el.classList.add('nhsuk-form-group--error')
-                        this.$refs["dayInput"].classList.add('nhsuk-input--error')
-                        this.$refs["monthInput"].classList.add('nhsuk-input--error')
-                        this.$refs["yearInput"].classList.add('nhsuk-input--error')
+                    if (errorMessage) {
+                        this.$el.classList.add("nhsuk-form-group--error");
+                        this.$refs["dayInput"].classList.add("nhsuk-input--error");
+                        this.$refs["monthInput"].classList.add("nhsuk-input--error");
+                        this.$refs["yearInput"].classList.add("nhsuk-input--error");
                         this.$refs["errorField"].classList.remove("hidden");
                     } else {
                         if (this.rank) {
-                            this.$emit('input', convertFormattedDateToDate(date));
+                            this.$emit("input", convertFormattedDateToDate(date));
                             this.$parent.datechanged(this.rank);
                         }
-                        this.$el.classList.remove('nhsuk-form-group--error')
-                        this.$refs["dayInput"].classList.remove('nhsuk-input--error')
-                        this.$refs["monthInput"].classList.remove('nhsuk-input--error')
-                        this.$refs["yearInput"].classList.remove('nhsuk-input--error')
+                        this.$el.classList.remove("nhsuk-form-group--error");
+                        this.$refs["dayInput"].classList.remove("nhsuk-input--error");
+                        this.$refs["monthInput"].classList.remove("nhsuk-input--error");
+                        this.$refs["yearInput"].classList.remove("nhsuk-input--error");
                         this.$refs["errorField"].classList.add("hidden");
                     }
 
-                    
+
 
                 })
                 .catch((error: any) => {
-                    console.log(error.response)
+                    console.log(error.response);
                 });
         },
-        getFormattedDate: function() {
+        getFormattedDate: function () {
             const dayInput = this.$refs["dayInput"];
             const monthInput = this.$refs["monthInput"];
             const yearInput = this.$refs["yearInput"];
@@ -79,15 +78,15 @@ const ValidateDate = Vue.extend({
             const monthValue = monthInput.value;
             const yearValue = yearInput.value;
 
-            if (dayValue === '' || monthValue === '' || yearValue === '') {
+            if (dayValue === "" || monthValue === "" || yearValue === "") {
                 return null;
             }
 
-            var formattedDate: FormattedDate = {
+            const formattedDate: FormattedDate = {
                 day: dayValue,
                 month: monthValue,
                 year: yearValue
-            }
+            };
 
             return formattedDate;
         }
--- ntbs-service/wwwroot/source/Components/ValidateMultiple.ts
+++ ntbs-service/wwwroot/source/Components/ValidateMultiple.ts
@@ -1,44 +1,56 @@
-import Vue from 'vue';
-import { getHeaders, getValidationPath } from '../helpers';
-import axios from 'axios';
+import Vue from "vue";
+import { getHeaders, getValidationPath } from "../helpers";
+import axios from "axios";
 
-const qs = require('qs');
+const qs = require("qs");
 
 const ValidateMultiple = Vue.extend({
     props: {
         model: String,
         properties: Array
     },
-    data: function() {
+    data: function () {
         return {
             hasError: false
-        }},
+        }
+    },
     methods: {
         // Annotate values to compare with ref="input[i]", and error fields with ref="errorField[i]", for any number of inputs that should be validated together
         // Each error message will be mapped to the corresponding field.
         validate: function () {
-            var inputs = this.createArrayFromRefElements('input');
-            var inputValues;
-            if (inputs[0].type == "radio") {
+            const inputs = this.createArrayFromRefElements("input");
+            var inputValues: any;
+            if (inputs[0].type === "radio") {
                 // TODO: Do this mapping for other types if element is reused for non-radio inputs.
                 inputValues = inputs.map((i: any) => i.checked);
             }
-            
-            var queryString = buildKeyValuePairsQueryString(this.properties, inputValues);
+
+            const queryString = buildKeyValuePairsQueryString(this.properties, inputValues);
             axios.get(`${getValidationPath(this.$props.model)}Properties?${queryString}`, { headers: getHeaders() })
-            .then((response: any) => {
-                var errorMessages = response.data;
-                this.hasError = errorMessages != '';
-                var errorFields = this.createArrayFromRefElements('errorField');
-                errorFields.forEach((errorField: any, index: number) => errorField.textContent = this.hasError ? errorMessages[index] : '');
-            })
-            .catch((error: any) => {
-                console.log(error.response)
-            });
+                .then((response: any) => {
+                    var errorMessages = response.data;
+                    this.hasError = !!errorMessages;
+                    var errorFields = this.createArrayFromRefElements("errorField");
+
+                    if (this.hasError) {
+                        errorFields.forEach((errorField: any, index: number) => {
+                            errorField.textContent = errorMessages[index];
+                            errorField.classList.remove("hidden");
+                        });
+                    } else {
+                        errorFields.forEach((errorField: any, index: number) => {
+                            errorField.textContent = "";
+                            errorField.classList.add("hidden");
+                        });
+                    }
+                })
+                .catch((error: any) => {
+                    console.log(error.response);
+                });
         },
-        createArrayFromRefElements: function(elementName: string) {
-            var array = [];
-            for (var i = 0; i < this.properties.length; i++) {
+        createArrayFromRefElements: function (elementName: string) {
+            const array = [];
+            for (let i = 0; i < this.properties.length; i++) {
                 array.push(this.$refs[`${elementName}[${i}]`]);
             }
             return array;
@@ -47,9 +59,9 @@ const ValidateMultiple = Vue.extend({
 });
 
 function buildKeyValuePairsQueryString(keys: Array<string>, values: Array<string>) {
-    var queryStringObject : any = { keyValuePairs: [] };
-    for (var i = 0; i < keys.length; i++) {
-        queryStringObject['keyValuePairs'].push( { key: keys[i], value: values[i] } );
+    const queryStringObject: any = { keyValuePairs: [] };
+    for (let i = 0; i < keys.length; i++) {
+        queryStringObject["keyValuePairs"].push({ key: keys[i], value: values[i] });
     }
     return qs.stringify(queryStringObject);
 };
