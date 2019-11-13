45af1db8272de69349f986c1040e202ba4b4b731
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-11-13 10:08:24 +0000

    Use config value to control dummy auth

--- ntbs-service/Program.cs
+++ ntbs-service/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using EFAuditer;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ntbs_service.Models;
