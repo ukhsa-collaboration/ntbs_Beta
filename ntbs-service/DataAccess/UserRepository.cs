ab7e44f21d4cf5212491a40ec4b383805a678baa
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-12-31 11:02:13 +0000

    NTBS-591 Use saving method that doesn't require detaching

--- ntbs-service/DataAccess/UserRepository.cs
+++ ntbs-service/DataAccess/UserRepository.cs
@@ -38,48 +38,24 @@ namespace ntbs_service.DataAccess
 
         private async Task AddUser(User user, IEnumerable<TBService> tbServices)
         {
-            AddCaseManagerTbServices(user, tbServices);
             _context.User.Add(user);
+            AddCaseManagerTbServices(user, tbServices);
             await _context.SaveChangesAsync();
         }
 
         private async Task UpdateUser(User existingUser, User newUser, IEnumerable<TBService> tbServices)
         {
-            DetachLocalTrackedObject(existingUser);
-
-            existingUser = newUser;
+            _context.Entry(existingUser).CurrentValues.SetValues(newUser);
             AddCaseManagerTbServices(existingUser, tbServices);
-            _context.Entry(existingUser).State = EntityState.Modified;
-
             await _context.SaveChangesAsync();
         }
 
-        private void DetachLocalTrackedObject(User existingUser)
+        private static void AddCaseManagerTbServices(User user, IEnumerable<TBService> tbServices)
         {
-            // Not sure why this is necessary/what is tracking the same entity twice, but without kept getting error:
-            // "The instance of entity type cannot be tracked because another instance of this type with the same key [Username] is already being tracked"
-            // Solution from https://stackoverflow.com/questions/36856073/the-instance-of-entity-type-cannot-be-tracked-because-another-instance-of-this-t
-            // TODO: Work out if there is a nicer way.
-            var local = _context.Set<User>()
-                            .Local
-                            .FirstOrDefault(entry => entry.Username.Equals(existingUser.Username));
-
-            if (local != null)
-            {
-                _context.Entry(local).State = EntityState.Detached;
-            }
-        }
-
-        private void AddCaseManagerTbServices(User user, IEnumerable<TBService> tbServices)
-        {
-            if (tbServices.Any())
-            {
-                user.CaseManagerTbServices = tbServices.Select(tb => new CaseManagerTbService { TbService = tb, CaseManager = user }).ToList();
-            }
-            else
-            {
-                user.CaseManagerTbServices = null;
-            }
+            var caseManagerTbServices = tbServices
+                .Select(tb => new CaseManagerTbService {TbService = tb, CaseManager = user})
+                .ToList();
+            user.CaseManagerTbServices = caseManagerTbServices.Any() ? caseManagerTbServices : null;
         }
     }
 }
