268b2f462fa7c417a1c3b2abf6a9a867189d8b7f
Conor Sheehan <Conor.Sheehan@softwire.com>
2020-01-07 12:21:18 +0000

    NTBS-378: Add functionality for linking to report page

---
+++ ntbs-service/Pages/Reports/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace ntbs_service
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string ReportType { get; set; }
+
+        public string ReportingUri { get; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            ReportingUri = configuration.GetSection("ExternalLinks")?["ReportingUri"];
+        }
+
+        public IActionResult OnGet()
+        {
+            if (!string.IsNullOrEmpty(ReportingUri))
+            {
+                return Redirect(ReportingUri);
+                // TODO: Add logic for routing to difference report types directly.
+            }
+
+            return Page();
+        }
+    }
+}
