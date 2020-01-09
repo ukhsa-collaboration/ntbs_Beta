8f4288f354b0fbb2f96aba772fd6010cbf40ae4e
ZOO\FIF <farman.farmanov@softwire.com>
2020-01-09 00:25:16 +0000

    NTBS-697: Add unmatch specimen functionality

--- ntbs-service/Services/SpecimenService.cs
+++ ntbs-service/Services/SpecimenService.cs
@@ -2,20 +2,22 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.Configuration;
-using System.Linq;
 using System.Collections.Generic;
 using ntbs_service.Models.Entities;
+using System.Data;
 
 namespace ntbs_service.Services
 {
     public interface ISpecimenService
     {
         Task<IEnumerable<Specimen>> GetSpecimenDetailsAsync(int notificationId);
+        Task UnmatchSpecimen(int notificationId, string labReferenceNumber);
     }
 
     public class SpecimenService : ISpecimenService
     {
-        private readonly string connectionString;
+        private readonly string reportingDbConnectionString;
+        private readonly string matchSpecimenDbConnectionString;
 
         private readonly string getMatchedSpecimenSqlFunction = @"
             SELECT NotificationId,
@@ -38,17 +40,32 @@ namespace ntbs_service.Services
                 PatientAddress
             FROM [dbo].[ufnGetMatchedSpecimen] (@notificationId)";
 
+        private readonly string unmatchSpecimentSqlProcedure = @"uspUnmatchSpecimen";
+
         public SpecimenService(IConfiguration _configuration)
         {
-            connectionString = _configuration.GetConnectionString("reporting");
+            reportingDbConnectionString = _configuration.GetConnectionString("reporting");
+            matchSpecimenDbConnectionString = _configuration.GetConnectionString("specimenMatching");
         }
 
         public async Task<IEnumerable<Specimen>> GetSpecimenDetailsAsync(int notificationId)
         {
-            using (var connection = new SqlConnection(connectionString))
+            using (var connection = new SqlConnection(reportingDbConnectionString))
             {
                 connection.Open();
                 return await connection.QueryAsync<Specimen>(getMatchedSpecimenSqlFunction, new { notificationId });
+            }
+        }
+
+        public async Task UnmatchSpecimen(int notificationId, string labReferenceNumber)
+        {
+            {
+                using (var connection = new SqlConnection(matchSpecimenDbConnectionString))
+                {
+                    connection.Open();
+                    // Add parameters when procedure is implemented properly
+                    await connection.QueryAsync(unmatchSpecimentSqlProcedure, commandType: CommandType.StoredProcedure);
+                }
             }
         }
     }
