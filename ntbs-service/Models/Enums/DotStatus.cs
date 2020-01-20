570f22a53101140666055da669d2dfb91c8879d2
ZOO\FIF <farman.farmanov@softwire.com>
2020-01-20 23:03:21 +0000

    NTBS-557: Add additional items on Clinical Details page for cohort review

---
+++ ntbs-service/Models/Enums/DotStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ntbs_service.Models.Enums
+{
+    public enum DotStatus
+    {
+        [Display(Name = "Dot received")]
+        DotReceived,
+        [Display(Name = "Dot refused")]
+        DotRefused,
+        [Display(Name = "Unknown")]
+        Unknown
+    }
+}
---
+++ ntbs-service/Models/Enums/HealthcareSetting.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ntbs_service.Models.Enums
+{
+    public enum HealthcareSetting
+    {
+        [Display(Name = "A&E")]
+        AccidentAndEmergency,
+        [Display(Name = "Contact Tracing")]
+        ContactTracing,
+        [Display(Name = "Find and Trace")]
+        FindAndTrace,
+        [Display(Name = "GP")]
+        GP,
+        [Display(Name = "Other")]
+        Other,
+        [Display(Name = "Unknown")]
+        Unknown
+    }
+}
