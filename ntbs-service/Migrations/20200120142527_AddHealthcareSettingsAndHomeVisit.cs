0f42f5f687b30623f597706bc067b6355c0206f0
ZOO\FIF <farman.farmanov@softwire.com>
2020-01-20 23:04:01 +0000

    NTBS-557: Add Health settings and home visit migration

---
+++ ntbs-service/Migrations/20200120142527_AddHealthcareSettingsAndHomeVisit.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class AddHealthcareSettingsAndHomeVisit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FirstHomeVisitDate",
+                table: "ClinicalDetails",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HealthcareDescription",
+                table: "ClinicalDetails",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HealthcareSetting",
+                table: "ClinicalDetails",
+                maxLength: 30,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HomeVisitCarriedOut",
+                table: "ClinicalDetails",
+                maxLength: 30,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstHomeVisitDate",
+                table: "ClinicalDetails");
+
+            migrationBuilder.DropColumn(
+                name: "HealthcareDescription",
+                table: "ClinicalDetails");
+
+            migrationBuilder.DropColumn(
+                name: "HealthcareSetting",
+                table: "ClinicalDetails");
+
+            migrationBuilder.DropColumn(
+                name: "HomeVisitCarriedOut",
+                table: "ClinicalDetails");
+        }
+    }
+}
