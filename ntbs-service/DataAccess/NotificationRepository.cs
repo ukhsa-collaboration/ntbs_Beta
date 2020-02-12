ddcebcc80bbfbaf2f6d52b36e04295f7a4a74cff
Christian McCaffery <christian.mccaffery@softwire.com>
2020-02-12 14:39:22 +0000

    NTBS-907 remove unnecessary using

--- ntbs-service/DataAccess/NotificationRepository.cs
+++ ntbs-service/DataAccess/NotificationRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query.Internal;
 using ntbs_service.Models;
 using ntbs_service.Models.Entities;
 using ntbs_service.Models.Enums;
