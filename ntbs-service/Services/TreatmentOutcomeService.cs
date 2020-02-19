d1d228ca568fb63e70f46129f2097ce5e426255a
Christian McCaffery <christian.mccaffery@softwire.com>
2020-02-19 09:29:07 +0000

    NTBS-923 add treatmentOutcomeService

---
+++ ntbs-service/Services/TreatmentOutcomeService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using ntbs_service.DataAccess;
+using ntbs_service.Models;
+using ntbs_service.Models.Entities;
+
+namespace ntbs_service.Services
+{
+    public interface ITreatmentOutcomeService
+    {
+    }
+
+    public class TreatmentOutcomeService : ITreatmentOutcomeService
+    {
+        private readonly IAlertRepository _alertRepository;
+
+        public TreatmentOutcomeService(
+            IAlertRepository alertRepository)
+        {
+            _alertRepository = alertRepository;
+        }
+
+        public async Task<bool> IsTreatmentOutcomeNeededAsync(IList<TreatmentEvent> treatmentEvents)
+        {
+            return false;
+        }
+    }
+}
