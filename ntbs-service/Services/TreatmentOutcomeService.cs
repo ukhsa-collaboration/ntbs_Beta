ddf6b2945980cfdf28b5fdedd3fe46ab751e56d1
Christian McCaffery <christian.mccaffery@softwire.com>
2020-02-19 17:15:54 +0000

    NTBS-923 add logic to IsTreatmentOutcomeNeeded

--- ntbs-service/Services/TreatmentOutcomeService.cs
+++ ntbs-service/Services/TreatmentOutcomeService.cs
@@ -3,11 +3,13 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Castle.Core.Internal;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using ntbs_service.DataAccess;
 using ntbs_service.Models;
 using ntbs_service.Models.Entities;
+using ntbs_service.Models.Enums;
 
 namespace ntbs_service.Services
 {
@@ -34,20 +36,46 @@ namespace ntbs_service.Services
             }
 
             var treatmentEventsBetweenOneAndTwoYears =
-                notification.TreatmentEvents.Where(t =>
-                    t.EventDate < (notification.ClinicalDetails.TreatmentStartDate ?? notification.NotificationDate)?.AddYears(2)
-                    && t.EventDate > (notification.ClinicalDetails.TreatmentStartDate ?? notification.NotificationDate)?.AddYears(1));
+                GetOrderedTreatmentEventsInWindowXtoXMinus1Years(notification, 2);
             
             if (yearsAfterTreatmentStartDate == 2)
             {
-                
+                return IsOutcomeNeededAt24Months(notification);
             }
-            
-            var treatmentEventsBetweenTwoAndThreeYears =
-                notification.TreatmentEvents.Where(t =>
-                    t.EventDate < (notification.ClinicalDetails.TreatmentStartDate ?? notification.NotificationDate)?.AddYears(3)
-                    && t.EventDate > (notification.ClinicalDetails.TreatmentStartDate ?? notification.NotificationDate)?.AddYears(2));
+
+            if (yearsAfterTreatmentStartDate == 3)
+            {
+                var treatmentEventsBetweenTwoAndThreeYears =
+                    GetOrderedTreatmentEventsInWindowXtoXMinus1Years(notification, 3);
+                if (treatmentEventsBetweenTwoAndThreeYears == null)
+                {
+                    return IsOutcomeNeededAt24Months(notification);
+                }
+            }
+
             return false;
+        }
+
+        public IEnumerable<TreatmentEvent> GetOrderedTreatmentEventsInWindowXtoXMinus1Years(Notification notification, int numberOfYears)
+        {
+            return notification.TreatmentEvents.Where(t =>
+                t.EventDate < (notification.ClinicalDetails.TreatmentStartDate ?? notification.NotificationDate)?.AddYears(numberOfYears)
+                && t.EventDate > (notification.ClinicalDetails.TreatmentStartDate ?? notification.NotificationDate)?.AddYears(numberOfYears - 1))
+                .OrderByDescending(t => t.EventDate);
+        }
+
+        private bool IsOutcomeNeededAt24Months(Notification notification)
+        {
+            var treatmentEventsBetweenOneAndTwoYears =
+                GetOrderedTreatmentEventsInWindowXtoXMinus1Years(notification, 2);
+            if (treatmentEventsBetweenOneAndTwoYears == null)
+            {
+                var mostRecentTreatmentEventBetweenZeroAndOneYears =
+                    GetOrderedTreatmentEventsInWindowXtoXMinus1Years(notification, 1).First();
+                return mostRecentTreatmentEventBetweenZeroAndOneYears.TreatmentOutcome.TreatmentOutcomeType ==
+                       TreatmentOutcomeType.NotEvaluated || !mostRecentTreatmentEventBetweenZeroAndOneYears.TreatmentEventTypeIsOutcome;
+            }
+            return treatmentEventsBetweenOneAndTwoYears.Last().TreatmentOutcome.TreatmentOutcomeType == TreatmentOutcomeType.NotEvaluated;
         }
     }
 }
