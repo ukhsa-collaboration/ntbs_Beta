a62c80472d7a97d38848bb0ad8c8fb9d30a29d77
ZOO\FIF <farman.farmanov@softwire.com>
2020-02-23 20:29:53 +0000

    NTBS-904: Show confirmation dialog before leaving the page without saving form

---
+++ ntbs-service/wwwroot/source/Components/ConfirmComponent.ts
@@ -0,0 +1,44 @@
+﻿import Vue from 'vue'
+
+const confirmTemplate = `
+    <div class="dialog-box">
+        <template>
+            <section>
+                <h3 class="dialog-box-message">{{ message }}</h3>
+                <div class="dialog-box-button-container">
+                    <button class="nhsuk-button nhsuk-button--secondary dialog-box-button" type="button" @click="rejectHandler">Stay</button>
+                    <button class="nhsuk-button nhsuk-button--secondary dialog-box-button" type="button" @click="resolveHandler">Leave</button>
+                </div>
+            </section>
+        </template>
+    </div>
+    `;
+
+export default Vue.extend({
+    name: 'ConfirmComponent',
+    template: confirmTemplate,
+    props: {
+        message: {
+            type: String,
+            required: true,
+        },
+        resolve: {
+            type: Function,
+            required: true,
+        },
+        reject: {
+            type: Function,
+            required: true,
+        },
+    },
+    methods: {
+        resolveHandler() {
+            // this value will be passed to `then`
+            this.resolve()
+        },
+        rejectHandler() {
+            // this value will be passed to `catch`
+            this.reject()
+        },
+    },
+});
---
+++ ntbs-service/wwwroot/source/Components/FormLeaveChecker.ts
@@ -0,0 +1,47 @@
+﻿import Vue from "vue";
+import ConfirmComponent from "./ConfirmComponent";
+
+const FormLeaveChecker = Vue.extend({
+    created(): void {
+        window.onclick = this.checkLeave.bind(this);
+    },
+    mounted: function() {
+        this.initialFormData = this.serialiseForm();
+    },
+    methods: {
+        beforeLoad: function () {
+            console.log(this.formDataJSON)
+        },
+        checkLeave: function (event : MouseEvent) {
+            const eventTarget = (event.target as HTMLLinkElement);
+            if (eventTarget.tagName.toLowerCase() === "a" && eventTarget.parentElement.dataset.ignoreSafeNavigationPopup !== "true") {
+                if (this.serialiseForm() !== this.initialFormData) {
+                    event.preventDefault();
+                    
+                    this.$modal.confirm(ConfirmComponent, 'Are you sure you want to leave the page without saving changes?')
+                        .then(() => {
+                            window.location.href = eventTarget.href;
+                        })
+                        .catch(() => {
+                        })
+                        .finally(() => {
+                            this.$modal.close()
+                        });
+                }
+            }
+        },
+        serialiseForm: function () {
+            const formData: any = new FormData(this.$refs["notificationForm"]);
+            const formEntries = Array.from(formData.entries());
+            
+            let stringifiedForm = "";
+            formEntries.forEach(function(entry: any) {
+                stringifiedForm += JSON.stringify(entry);
+            });
+            
+            return stringifiedForm;
+        }
+    }
+});
+
+export default FormLeaveChecker;
