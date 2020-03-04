faecc3663c0ad881a63e2f1bac4141bbe438cc8c
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2020-03-04 09:27:17 +0000

    NTBS-861 Add new ETS country

---
+++ ntbs-service/Migrations/20200304092251_AddNewEtsHosptial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class AddNewEtsHosptial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hospital",
+                columns: new[] { "HospitalId", "CountryCode", "IsLegacy", "Name", "TBServiceCode" },
+                values: new object[] { new Guid("b93a5546-6c26-4a4b-a2fa-427b2711b861"), "E92000001", false, "SOUTH WEST ACUTE HOSPITAL", "TBS0123" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hospital",
+                keyColumn: "HospitalId",
+                keyValue: new Guid("b93a5546-6c26-4a4b-a2fa-427b2711b861"));
+        }
+    }
+}
