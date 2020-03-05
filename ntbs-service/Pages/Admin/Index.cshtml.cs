32354f856a320e9cdb435ed39de54064d086e567
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2020-03-05 19:56:12 +0000

    Add admin page

---
+++ ntbs-service/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ntbs_service.Pages.Admin
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
