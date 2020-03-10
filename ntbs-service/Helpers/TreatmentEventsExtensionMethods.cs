04c2701e05aaf3c7f6083c91484e5105b954c0eb
Christian McCaffery <christian.mccaffery@softwire.com>
2020-03-10 10:09:38 +0000

    NTBS-454 order treatment events on the same day

--- ntbs-service/Helpers/TreatmentEventsExtensionMethods.cs
+++ ntbs-service/Helpers/TreatmentEventsExtensionMethods.cs
@@ -20,7 +20,7 @@ namespace ntbs_service.Helpers
         
         public static Dictionary<int, List<TreatmentEvent>> GroupByEpisode(this IEnumerable<TreatmentEvent> treatmentEvents)
         {
-            var orderedTreatmentEvents = treatmentEvents.OrderBy(t => t.EventDate);
+            var orderedTreatmentEvents = treatmentEvents.OrderBy(t => t.EventDate).ThenBy(t => t.TreatmentEventTypeIsOutcome);
             var groupedEpisodes = new Dictionary<int, List<TreatmentEvent>>();
             var episodeCount = 1;
             
