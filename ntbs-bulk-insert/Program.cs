b12d00a7ff91c2b1bdf1f1234ae4d151005395e9
Christian McCaffery <christian.mccaffery@softwire.com>
2020-04-01 10:16:27 +0100

    NTBS-1038 Add required fields with nbuilder to notifications being bulk inserted

--- ntbs-bulk-insert/Program.cs
+++ ntbs-bulk-insert/Program.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using ntbs_service.DataAccess;
 using ntbs_service.Models.Entities;
 using ntbs_service.Models.Enums;
+using ntbs_service.Models.ReferenceEntities;
 
 namespace ConsoleApp2
 {
@@ -54,18 +55,37 @@ namespace ConsoleApp2
             var numberOfNotifications = 3000;
             var rand = new Random();
             var tbServices = (await context.TbService.ToListAsync()).Select(t => t.Code).ToList();
+            var hospitals = (await context.Hospital.ToListAsync());
+            
             var notificationsOperable = Builder<Notification>.CreateListOfSize(numberOfNotifications)
                 .All()
                 .With(n => n.NotificationId = 0)
                 .With(n => n.NotificationStatus = NotificationStatus.Notified)
                 .With(n => n.PatientDetails.GivenName = Faker.Name.First())
                 .With(n => n.PatientDetails.FamilyName = Faker.Name.Last())
+                .With(n => n.PatientDetails.NoFixedAbode = true)
+                .With(n => n.TestData.HasTestCarriedOut = false)
                 .With(n => n.ClinicalDetails.Notes = "UniqueBulkInsert")
                 .With(n => n.GroupId = null)
                 // Add randomised fields that Faker cannot generate
+                .With(n => n.PatientDetails.Dob = AddRandomDateTimeBetween1950And2000(rand))
+                .With(n => n.PatientDetails.SexId = rand.Next(1, 3))
+                .With(n => n.PatientDetails.EthnicityId = rand.Next(1, 10))
+                .With(n => n.PatientDetails.CountryId = rand.Next(1, 200))
                 .With(n => n.NotificationDate = AddRandomDateTimeBetween2014And2017(rand))
                 .With(n => n.HospitalDetails.TBServiceCode = AddRandomTbService(rand, tbServices))
-                .With(n => n.PatientDetails.NhsNumber = AddRandomTestNhsNumber(rand));
+                .With(n => n.HospitalDetails.HospitalId = hospitals.FirstOrDefault(h => h.TBServiceCode == n.HospitalDetails.TBServiceCode)?.HospitalId)
+                .With(n => n.PatientDetails.NhsNumber = AddRandomTestNhsNumber(rand))
+                .With(n => n.NotificationSites = new List<NotificationSite>
+                {
+                    new NotificationSite
+                    {
+                        SiteId = 1
+                        
+                    }
+                })
+                .With(n => n.ClinicalDetails.DiagnosisDate = new DateTime(2014, 1, 1));
+            
 
             if (addTreatmentEvents)
             {
@@ -82,6 +102,12 @@ namespace ConsoleApp2
         {
             var days = rand.Next(1, 1000);
             return new DateTime(2014, 1, 1).AddDays(days);
+        }
+        
+        private static DateTime AddRandomDateTimeBetween1950And2000(Random rand)
+        {
+            var days = rand.Next(1, 18262);
+            return new DateTime(1950, 1, 1).AddDays(days);
         }
 
         private static string AddRandomTbService(Random rand, IList<string> tbServices)
