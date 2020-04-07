ac6b63c24f9658288fa99b7e0f7b1dca0aeeac01
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2020-04-07 16:06:37 +0100

    NTBS-1066 Update South West Acute Hospital data

---
+++ ntbs-service/Migrations/20200407145727_UpdateSouthWestAcuteHospitalData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class UpdateSouthWestAcuteHospitalData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "ReferenceData",
+                table: "TbService",
+                columns: new[] { "Code", "IsLegacy", "Name", "PHECCode", "ServiceAdGroup" },
+                values: new object[] { "TBS1000", false, "SOUTH WEST ACUTE HOSPITAL", "PHECNI", null });
+
+            migrationBuilder.UpdateData(
+                schema: "ReferenceData",
+                table: "Hospital",
+                keyColumn: "HospitalId",
+                keyValue: new Guid("b93a5546-6c26-4a4b-a2fa-427b2711b861"),
+                columns: new[] { "CountryCode", "TBServiceCode" },
+                values: new object[] { "N92000002", "TBS1000" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "ReferenceData",
+                table: "TbService",
+                keyColumn: "Code",
+                keyValue: "TBS1000");
+
+            migrationBuilder.UpdateData(
+                schema: "ReferenceData",
+                table: "Hospital",
+                keyColumn: "HospitalId",
+                keyValue: new Guid("b93a5546-6c26-4a4b-a2fa-427b2711b861"),
+                columns: new[] { "CountryCode", "TBServiceCode" },
+                values: new object[] { "E92000001", "TBS0123" });
+        }
+    }
+}
