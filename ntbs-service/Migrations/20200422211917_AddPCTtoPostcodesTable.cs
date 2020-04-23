851f587579f33206a3b933806127f179cfb0092a
ZOO\FIF <farman.farmanov@softwire.com>
2020-04-23 14:23:26 +0100

    NTBS-1327: Add PCT to postcodes table

---
+++ ntbs-service/Migrations/20200422211917_AddPCTtoPostcodesTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class AddPCTtoPostcodesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE [ReferenceData].PostcodeLookup ADD PCT varchar(10)");
+            
+            var pathToFile = Path.Combine(Environment.CurrentDirectory, "Models/SeedData/PostcodePCT.csv");
+            const int itemsPerUpdate = 1000;
+
+            var postcodeArray = new string[itemsPerUpdate];
+            var pctArray = new string[itemsPerUpdate];
+
+            int index = 0;
+
+            using (StreamReader reader = new StreamReader(pathToFile)) 
+            using (CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture)) {
+                csvReader.Read();
+                csvReader.ReadHeader();
+                while (csvReader.Read())
+                {
+                    postcodeArray[index] = csvReader.GetField<string>("POSTCODE");
+                    pctArray[index] = csvReader.GetField<string>("PCT");
+                    index++;
+
+                    if (index == itemsPerUpdate) 
+                    {
+                        migrationBuilder.UpdateData(
+                            table: "PostcodeLookup",
+                            schema: "ReferenceData",
+                            keyColumn: "Postcode",
+                            keyValues: postcodeArray,
+                            column: "PCT",
+                            values: pctArray
+                        );
+                        postcodeArray = new string[itemsPerUpdate];
+                        pctArray = new string[itemsPerUpdate];
+                        index = 0;
+                    }
+                }
+            }
+
+            if (index > 0) 
+            {
+                migrationBuilder.UpdateData(
+                    table: "PostcodeLookup",
+                    schema: "ReferenceData",
+                    keyColumn: "Postcode",
+                    keyValues: postcodeArray,
+                    column: "PCT",
+                    values: pctArray
+                );             
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"ALTER TABLE ReferenceData.PostcodeLookup DROP PCT");
+        }
+    }
+}
