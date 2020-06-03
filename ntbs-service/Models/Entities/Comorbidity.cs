6b01725cd3bd119eb769d9b2694b98be5a0a0a8a
ZOO\FIF <farman.farmanov@softwire.com>
2020-06-03 23:05:54 +0100

    NTBS-1353: Capitalise first letter of the disease

--- ntbs-service/Models/Entities/Comorbidity.cs
+++ ntbs-service/Models/Entities/Comorbidity.cs
@@ -8,15 +8,15 @@ namespace ntbs_service.Models.Entities
     [Owned]
     public class ComorbidityDetails : ModelBase, IOwnedEntityForAuditing
     {
-        [Display(Name = "Does the patient have diabetes?")]
+        [Display(Name = "Does the patient have Diabetes?")]
         public Status? DiabetesStatus { get; set; }
         [Display(Name = "Does the patient have Hepatitis B?")]
         public Status? HepatitisBStatus { get; set; }
         [Display(Name = "Does the patient have Hepatitis C?")]
         public Status? HepatitisCStatus { get; set; }
-        [Display(Name = "Does the patient have chronic liver disease?")]
+        [Display(Name = "Does the patient have Chronic liver disease?")]
         public Status? LiverDiseaseStatus { get; set; }
-        [Display(Name = "Does the patient have chronic renal disease?")]
+        [Display(Name = "Does the patient have Chronic renal disease?")]
         public Status? RenalDiseaseStatus { get; set; }
 
         string IOwnedEntityForAuditing.RootEntityType => RootEntities.Notification;
