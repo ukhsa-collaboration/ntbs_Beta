7af173971190e1437ff8c6eaadee23a6568f015d
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2020-07-21 10:54:16 +0100

    Add a whoami page listing claims

---
+++ ntbs-service/Pages/WhoAmI.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ntbs_service.Pages
+{
+    public class WhoAmI : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
