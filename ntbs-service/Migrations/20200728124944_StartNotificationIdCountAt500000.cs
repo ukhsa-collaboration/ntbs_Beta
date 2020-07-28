45790deeb4d2667198cf8752c33a34c0c76c78a8
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2020-07-28 14:04:58 +0100

    NTBS-1688 Set notification id starting number at 400 000

---
+++ ntbs-service/Migrations/20200728124944_StartNotificationIdCountAt500000.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class StartNotificationIdCountAt500000 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // This is to stay wall clear of potential clashes with ETS ids which are also incrementing
+            // integers (last ones seen at the time of writing are 244851)
+            migrationBuilder.Sql("DBCC CHECKIDENT ('Notification', RESEED, 400000)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
