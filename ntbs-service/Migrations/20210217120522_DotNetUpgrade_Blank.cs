e27936d0085efebcd2e677d688f2e88a2430e46a
LeoCie <leon.cieslik@softwire.com>
2021-02-18 11:36:34 +0000

    NTBS-2074: Add comment describing why we added migration

--- ntbs-service/Migrations/20210217120522_DotNetUpgrade_Blank.cs
+++ ntbs-service/Migrations/20210217120522_DotNetUpgrade_Blank.cs
@@ -2,6 +2,8 @@
 
 namespace ntbs_service.Migrations
 {
+    // This migration is here to represent mostly trivial changes made to our
+    // context model snapshot after the upgrade to .NET 5.0
     public partial class DotNetUpgrade_Blank : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
