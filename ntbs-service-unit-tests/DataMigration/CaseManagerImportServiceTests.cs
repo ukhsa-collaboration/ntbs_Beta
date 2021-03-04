77b1c6ab2aa0a9aab7133a6c4485d238b57d20a3
LeoCie <leon.cieslik@softwire.com>
2021-03-04 17:16:48 +0000

    NTBS-2110: Refactor case manager import service tests

--- ntbs-service-unit-tests/DataMigration/CaseManagerImportServiceTests.cs
+++ ntbs-service-unit-tests/DataMigration/CaseManagerImportServiceTests.cs
@@ -13,19 +13,23 @@ using Xunit;
 
 namespace ntbs_service_unit_tests.DataMigration
 {
-    public class CaseManagerImportServiceTests : IClassFixture<CaseManagerImportServiceTests.DatabaseFixture>
+    public class CaseManagerImportServiceTests : IDisposable
     {
+        private const string NOTIFICATION_ID = "11111";
+        private const string CASE_MANAGER_USERNAME = "TestUser@nhs.net";
+        private static readonly Guid HOSPITAL_GUID = new Guid("B8AA918D-233F-4C41-B9AE-BE8A8DC8BE7A");
+
+        private readonly NtbsContext _context;
         private readonly CaseManagerImportService _caseManagerImportService;
         private readonly Mock<IMigrationRepository> _migrationRepositoryMock = new Mock<IMigrationRepository>();
 
         private Dictionary<string, IEnumerable<MigrationDbNotification>> _idToNotificationDict;
         private Dictionary<string, MigrationLegacyUser> _usernameToLegacyUserDict;
         private Dictionary<string, IEnumerable<MigrationLegacyUserHospital>> _usernameToLegacyUserHospitalDict;
-        private readonly NtbsContext _context;
 
-        public CaseManagerImportServiceTests(DatabaseFixture fixture)
+        public CaseManagerImportServiceTests()
         {
-            _context = fixture.Context;
+            _context = SetupTestContext();
             SetupMockMigrationRepo();
             IUserRepository userRepository = new UserRepository(_context);
             IReferenceDataRepository referenceDataRepository = new ReferenceDataRepository(_context);
@@ -34,82 +38,19 @@ namespace ntbs_service_unit_tests.DataMigration
                 _migrationRepositoryMock.Object, importLogger);
         }
 
-        [Fact]
-        public async Task WhenCaseManagerForLegacyNotificationDoesNotExistInNtbs_AddsCaseManager()
+        public void Dispose()
         {
-            // Arrange
-            var notification = new Notification
-            {
-                IsLegacy = true,
-                LTBRID = "11111",
-                HospitalDetails = new HospitalDetails {TBServiceCode = "TBS00TEST"}
-            };
-            _idToNotificationDict = new Dictionary<string, IEnumerable<MigrationDbNotification>>
-            {
-                {
-                    "11111",
-                    new List<MigrationDbNotification> {new MigrationDbNotification {CaseManager = "TestUser@nhs.net"}}
-                }
-            };
-            _usernameToLegacyUserDict = new Dictionary<string, MigrationLegacyUser>
-            {
-                {
-                    "TestUser@nhs.net",
-                    new MigrationLegacyUser {Username = "TestUser@nhs.net", GivenName = "John", FamilyName = "Johnston"}
-                }
-            };
-            _usernameToLegacyUserHospitalDict = new Dictionary<string, IEnumerable<MigrationLegacyUserHospital>> 
-                {{ "TestUser@nhs.net", new List<MigrationLegacyUserHospital>() }};
-
-            // Act
-            await _caseManagerImportService.ImportOrUpdateCaseManager(notification, null, "test-request-1");
-
-            // Assert
-            var addedUser = _context.User.SingleOrDefault();
-            Assert.NotNull(addedUser);
-            Assert.Equal("John", addedUser.GivenName);
-            Assert.Equal("Johnston", addedUser.FamilyName);
-            Assert.False(addedUser.IsActive);
-            Assert.False(addedUser.IsCaseManager);
-            Assert.DoesNotContain("TBS00TEST", addedUser.CaseManagerTbServices.Select(cmtb => cmtb.TbServiceCode));
+            _context.Dispose();
         }
 
         [Fact]
-        public async Task WhenCaseManagerForLegacyNotificationDoesNotExistAndHasCorrectPermissions_AddsCaseManagerTbServices()
+        public async Task WhenCaseManagerForLegacyNotificationWithCorrectPermissionsDoesNotExistInNtbs_AddsCaseManagerWithTbServices()
         {
             // Arrange
-            var notification = new Notification
-            {
-                IsLegacy = true,
-                LTBRID = "11111",
-                HospitalDetails = new HospitalDetails {TBServiceCode = "TBS00TEST"}
-            };
-            _idToNotificationDict = new Dictionary<string, IEnumerable<MigrationDbNotification>>
-            {
-                {
-                    "11111",
-                    new List<MigrationDbNotification> {new MigrationDbNotification {CaseManager = "TestUser@nhs.net"}}
-                }
-            };
-            _usernameToLegacyUserDict = new Dictionary<string, MigrationLegacyUser>
-            {
-                {
-                    "TestUser@nhs.net",
-                    new MigrationLegacyUser {Username = "TestUser@nhs.net", GivenName = "John", FamilyName = "Johnston"}
-                }
-            };
-            _usernameToLegacyUserHospitalDict = new Dictionary<string, IEnumerable<MigrationLegacyUserHospital>>
-            {
-                {
-                    "TestUser@nhs.net",
-                    new List<MigrationLegacyUserHospital>
-                    {
-                        new MigrationLegacyUserHospital {HospitalId = new Guid("B8AA918D-233F-4C41-B9AE-BE8A8DC8BE7A")}
-                    }
-                }
-            };
-            await _context.Hospital.AddAsync(new Hospital{HospitalId = new Guid("B8AA918D-233F-4C41-B9AE-BE8A8DC8BE7A"), TBService = new TBService{Code = "TBS00TEST"}});
-            await _context.SaveChangesAsync();
+            
+            var notification = GivenLegacyNotificationWithTbServiceCode("TBS00TEST");
+            GivenLegacyUserWithName("John", "Johnston");
+            await GivenLegacyUserHasPermissionsForTbService("TBS00TEST");
 
             // Act
             await _caseManagerImportService.ImportOrUpdateCaseManager(notification, null, "test-request-1");
@@ -125,39 +66,40 @@ namespace ntbs_service_unit_tests.DataMigration
         }
 
         [Fact]
-        public async Task WhenCaseManagerForLegacyNotificationDoesExist_UpdatesName()
+        public async Task WhenCaseManagerForLegacyNotificationWithNoPermissionsDoesNotExistInNtbs_AddsCaseManagerWithNoTbServices()
         {
             // Arrange
-            var notification = new Notification
-            {
-                IsLegacy = true,
-                LTBRID = "11111",
-                HospitalDetails = new HospitalDetails {TBServiceCode = "TBS00TEST"}
-            };
-            _idToNotificationDict = new Dictionary<string, IEnumerable<MigrationDbNotification>>
-            {
-                {
-                    "11111",
-                    new List<MigrationDbNotification> {new MigrationDbNotification {CaseManager = "TestUser@nhs.net"}}
-                }
-            };
-            _usernameToLegacyUserDict = new Dictionary<string, MigrationLegacyUser>
-            {
-                {
-                    "TestUser@nhs.net",
-                    new MigrationLegacyUser {Username = "TestUser@nhs.net", GivenName = "John", FamilyName = "Johnston"}
-                }
-            };
-            _usernameToLegacyUserHospitalDict = new Dictionary<string, IEnumerable<MigrationLegacyUserHospital>> 
-                {{ "TestUser@nhs.net", new List<MigrationLegacyUserHospital>() }};
-            await _context.User.AddAsync(new User {GivenName = "Jon", FamilyName = "Jonston", Username = "TestUser@nhs.net"});
-            await _context.SaveChangesAsync();
+            var notification = GivenLegacyNotificationWithTbServiceCode("TBS00TEST");
+            GivenLegacyUserWithName("John", "Johnston");
+            await GivenLegacyUserHasPermissionsForTbService("TBS11FAKE");
 
             // Act
             await _caseManagerImportService.ImportOrUpdateCaseManager(notification, null, "test-request-1");
 
             // Assert
-            var updatedUser = _context.User.SingleOrDefault();
+            var addedUser = _context.User.SingleOrDefault();
+            Assert.NotNull(addedUser);
+            Assert.Equal("John", addedUser.GivenName);
+            Assert.Equal("Johnston", addedUser.FamilyName);
+            Assert.False(addedUser.IsActive);
+            Assert.False(addedUser.IsCaseManager);
+            Assert.DoesNotContain("TBS00TEST", addedUser.CaseManagerTbServices.Select(cmtb => cmtb.TbServiceCode));
+        }
+
+        [Fact]
+        public async Task WhenCaseManagerForLegacyNotificationExistsInNtbs_UserNotImportedButNameUpdated()
+        {
+            // Arrange
+            var notification = GivenLegacyNotificationWithTbServiceCode("TBS00TEST");
+            GivenLegacyUserWithName("John", "Johnston");
+            await GivenLegacyUserHasPermissionsForTbService("TBS99HULL");
+            await GivenUserExistsInNtbsWithName("Jon", "Jonston");
+
+            // Act
+            await _caseManagerImportService.ImportOrUpdateCaseManager(notification, null, "test-request-1");
+
+            // Assert
+            var updatedUser = _context.User.Single();
             Assert.NotNull(updatedUser);
             Assert.Equal("John", updatedUser.GivenName);
             Assert.Equal("Johnston", updatedUser.FamilyName);
@@ -173,22 +115,65 @@ namespace ntbs_service_unit_tests.DataMigration
                 .Returns((List<string> ids) => Task.FromResult(_idToNotificationDict[ids[0]]));
         }
 
-        public class DatabaseFixture : IDisposable
+        private Notification GivenLegacyNotificationWithTbServiceCode(string TbServiceCode)
         {
-            public DatabaseFixture()
+            _idToNotificationDict = new Dictionary<string, IEnumerable<MigrationDbNotification>>
             {
-                Context = new NtbsContext(new DbContextOptionsBuilder<NtbsContext>()
-                    .UseInMemoryDatabase(nameof(CaseManagerImportService))
-                    .Options
-                );
-            }
-
-            public void Dispose()
+                {
+                    NOTIFICATION_ID,
+                    new List<MigrationDbNotification> {new MigrationDbNotification {CaseManager = CASE_MANAGER_USERNAME}}
+                }
+            };
+            return new Notification
             {
-                Context.Dispose();
-            }
+                IsLegacy = true,
+                LTBRID = NOTIFICATION_ID,
+                HospitalDetails = new HospitalDetails {TBServiceCode = TbServiceCode}
+            };
+        }
 
-            public NtbsContext Context { get; private set; }
+        private void GivenLegacyUserWithName(string givenName, string familyName)
+        {
+            _usernameToLegacyUserDict = new Dictionary<string, MigrationLegacyUser>
+            {
+                {
+                    CASE_MANAGER_USERNAME,
+                    new MigrationLegacyUser {Username = CASE_MANAGER_USERNAME, GivenName = givenName, FamilyName = familyName}
+                }
+            };
+        }
+
+        private async Task GivenLegacyUserHasPermissionsForTbService(string tbServiceCode)
+        {
+            _usernameToLegacyUserHospitalDict = new Dictionary<string, IEnumerable<MigrationLegacyUserHospital>>
+            {
+                {
+                    CASE_MANAGER_USERNAME,
+                    new List<MigrationLegacyUserHospital>
+                    {
+                        new MigrationLegacyUserHospital {HospitalId = HOSPITAL_GUID}
+                    }
+                }
+            };
+            await _context.Hospital.AddAsync(new Hospital{HospitalId = HOSPITAL_GUID, TBService = new TBService{Code = tbServiceCode}});
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task GivenUserExistsInNtbsWithName(string givenName, string familyName)
+        {
+            await _context.User.AddAsync(
+                new User {GivenName = givenName, FamilyName = familyName, Username = CASE_MANAGER_USERNAME, IsActive = true});
+            await _context.SaveChangesAsync();
+        }
+
+        private NtbsContext SetupTestContext()
+        {
+            // Generating a unique database name makes sure the database is not shared between tests.
+            string dbName = Guid.NewGuid().ToString();
+            return new NtbsContext(new DbContextOptionsBuilder<NtbsContext>()
+                .UseInMemoryDatabase(dbName)
+                .Options
+            );
         }
     }
 }
