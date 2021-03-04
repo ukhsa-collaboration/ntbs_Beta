1d9e369d579e9d03d6e179b98f8bd3ac3362a94d
LeoCie <leon.cieslik@softwire.com>
2021-03-04 16:07:41 +0000

    NTBS-1743: Add assertion in test for first home visit date

--- ntbs-service-unit-tests/DataMigration/NotificationMapperTest.cs
+++ ntbs-service-unit-tests/DataMigration/NotificationMapperTest.cs
@@ -116,6 +116,7 @@ namespace ntbs_service_unit_tests.DataMigration
 
             Assert.Equal(HIVTestStatus.HIVStatusKnown, notification.ClinicalDetails.HIVTestState);
             Assert.Equal(Status.Yes, notification.ClinicalDetails.HomeVisitCarriedOut);
+            Assert.Equal(new DateTime(2015,03,25, 00,00,00), notification.ClinicalDetails.FirstHomeVisitDate);
             Assert.Equal("Patient did not begin course of treatment under DOT", notification.ClinicalDetails.Notes);
             
             Assert.Equal(Status.No, notification.SocialRiskFactors.RiskFactorDrugs.Status);
