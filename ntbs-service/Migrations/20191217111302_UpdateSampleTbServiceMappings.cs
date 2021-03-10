141922cd903377f5a5ac296d17c0f19ea6454045
Joshua Cadney <joshua.cadney@softwire.com>
2021-03-10 08:48:48 +0000

    NTBS-NONE Fix issues with migrations caused by .NET 5 upgrade

--- ntbs-service/Migrations/20191217111302_UpdateSampleTbServiceMappings.cs
+++ ntbs-service/Migrations/20191217111302_UpdateSampleTbServiceMappings.cs
@@ -7,20 +7,20 @@ namespace ntbs_service.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             //                          table, key column,   key value, change column, change value
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0040", "ServiceAdGroup", "Global.NIS.NTBS.Service_Hull");
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0107", "ServiceAdGroup", "Global.NIS.NTBS.Service_Leicester");
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0116", "ServiceAdGroup", "Global.NIS.NTBS.Service_Luton");
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0178", "ServiceAdGroup", "Global.NIS.NTBS.Service_Bolton");
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0040", "ServiceAdGroup", "Global.NIS.NTBS.Service_Hull");
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0107", "ServiceAdGroup", "Global.NIS.NTBS.Service_Leicester");
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0116", "ServiceAdGroup", "Global.NIS.NTBS.Service_Luton");
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0178", "ServiceAdGroup", "Global.NIS.NTBS.Service_Bolton");
 
             migrationBuilder.UpdateData("PHEC", "Code", "PHECNI", "AdGroup", "Global.NIS.NTBS.NI");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0040", "ServiceAdGroup", null);
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0107", "ServiceAdGroup", null);
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0116", "ServiceAdGroup", null);
-            migrationBuilder.UpdateData("TBService", "Code", "TBS0178", "ServiceAdGroup", null);
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0040", "ServiceAdGroup", null);
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0107", "ServiceAdGroup", null);
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0116", "ServiceAdGroup", null);
+            migrationBuilder.UpdateData("TbService", "Code", "TBS0178", "ServiceAdGroup", null);
 
             migrationBuilder.UpdateData("PHEC", "Code", "PHECNI", "AdGroup", null);
         }
