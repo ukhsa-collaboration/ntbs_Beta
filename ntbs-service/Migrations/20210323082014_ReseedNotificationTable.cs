8d1657e5c0da095dec990dedf88c8ff973dbeaa1
Joshua Cadney <joshua.cadney@softwire.com>
2021-03-23 08:26:26 +0000

    NTBS-2150 Reseed Notification table to 325000 by default

---
+++ ntbs-service/Migrations/20210323082014_ReseedNotificationTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class ReseedNotificationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF ((SELECT last_value FROM sys.identity_columns WHERE object_id = OBJECT_ID('Notification')) < 325000)
+                    DBCC CHECKIDENT('Notification', RESEED, 325000);
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Do nothing
+        }
+    }
+}
