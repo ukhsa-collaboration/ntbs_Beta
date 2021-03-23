0618972372223e41470d642c0d2f35e36f240762
Joshua Cadney <joshua.cadney@softwire.com>
2021-03-23 11:42:09 +0000

    NTBS-2150 Use 300,000 as the start of the test data range

--- ntbs-service/Migrations/20210323082014_ReseedNotificationTable.cs
+++ ntbs-service/Migrations/20210323082014_ReseedNotificationTable.cs
@@ -7,8 +7,8 @@ namespace ntbs_service.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                IF ((SELECT last_value FROM sys.identity_columns WHERE object_id = OBJECT_ID('Notification')) < 325000)
-                    DBCC CHECKIDENT('Notification', RESEED, 325000);
+                IF ((SELECT last_value FROM sys.identity_columns WHERE object_id = OBJECT_ID('Notification')) < 300000)
+                    DBCC CHECKIDENT('Notification', RESEED, 300000);
             ");
         }
 
