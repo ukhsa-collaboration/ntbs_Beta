ef6af2cd842734f104dc139c7edf80475e51c88b
Joshua Cadney <joshua.cadney@softwire.com>
2021-04-13 17:59:22 +0100

    NTBS-2116 Run UI tests through github action

--- ntbs-ui-tests/test.ps1
+++ ntbs-ui-tests/test.ps1
@@ -1,8 +1,13 @@
 #!/usr/local/bin/powershell
 
-.\node_modules\.bin\selenium-standalone.ps1 install
+.\node_modules\.bin\selenium-standalone.ps1 install --drivers.chrome.version=89.0.4389.23
 
-$SeleniumProcess = Start-Process powershell -Argument "node_modules/.bin/selenium-standalone.ps1 start" -PassThru
+$SeleniumProcess = Start-Process powershell -Argument "node_modules/.bin/selenium-standalone.ps1 start --drivers.chrome.version=89.0.4389.23" -PassThru
+
+Get-Process -Id $SeleniumProcess.Id
+
+Start-Sleep -s 10
+
 try {
     dotnet test
 }
