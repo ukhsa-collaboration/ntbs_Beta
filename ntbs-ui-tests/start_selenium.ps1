c64b186afaf2acde65cd3f67330e7485e4db3fc0
Joshua Cadney <joshua.cadney@softwire.com>
2021-04-16 11:08:02 +0100

    NTBS-2116 Remove debugging information from powershell scripts

--- ntbs-ui-tests/start_selenium.ps1
+++ ntbs-ui-tests/start_selenium.ps1
@@ -1,17 +1,5 @@
 #!/usr/local/bin/powershell
 
-ping 127.0.0.1
+.\node_modules\.bin\selenium-standalone.ps1 install
 
-telnet host 4444
-netstat -an
-
-.\node_modules\.bin\selenium-standalone.ps1 install --drivers.chrome.version=89.0.4389.23
-
-netstat -an
-
-try {
-    .\node_modules\.bin\selenium-standalone.ps1 start --drivers.chrome.version=89.0.4389.23
-}
-finally {
-    netstat -an
-}
+.\node_modules\.bin\selenium-standalone.ps1 start
--- ntbs-ui-tests/test.ps1
+++ ntbs-ui-tests/test.ps1
@@ -1,12 +1,8 @@
 #!/usr/local/bin/powershell
 
-.\node_modules\.bin\selenium-standalone.ps1 install --drivers.chrome.version=89.0.4389.23
+.\node_modules\.bin\selenium-standalone.ps1 install
 
-$SeleniumProcess = Start-Process powershell -Argument "node_modules/.bin/selenium-standalone.ps1 start --drivers.chrome.version=89.0.4389.23" -PassThru
-
-Get-Process -Id $SeleniumProcess.Id
-
-Start-Sleep -s 10
+$SeleniumProcess = Start-Process powershell -Argument "node_modules/.bin/selenium-standalone.ps1 start" -PassThru
 
 try {
     dotnet test
