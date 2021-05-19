3e19124bbf95fe934e5d796e795ff0ab2f526ad8
Benji Marshall <Benji.Marshall@softwire.com>
2021-05-19 13:36:55 +0100

    Add LegacyImportMigrationRun DB tables
    Add tables to track the results of a legacy migration import run.
    Add:
     - LegacyImportMigrationRun: metadata about a migration import run.
     - LegacyImportNotificationLogMessage: warning and error messages for a
    specific notification during a migration run.
     - LegacyImportNotificationOutcome: tracking success or failure when
    migrating a notification during a migration run.

---
+++ ntbs-service/Models/Entities/Alerts/LegacyImportNotificationOutcome.cs
@@ -0,0 +1,12 @@
+﻿namespace ntbs_service.Models.Entities.Alerts
+{
+    public class LegacyImportNotificationOutcome
+    {
+        public int LegacyImportNotificationOutcomeId { get; set; }
+        public int LegacyImportMigrationRunId { get; set; }
+        public string OldNotificationId { get; set; }
+        public int? NtbsId { get; set; }
+        public bool? SuccessfullyMigrated { get; set; }
+        public string Notes { get; set; }
+    }
+}
---
+++ ntbs-service/Models/Entities/LegacyImportNotificationLogMessage.cs
@@ -0,0 +1,13 @@
+﻿using ntbs_service.Models.Enums;
+
+namespace ntbs_service.Models.Entities
+{
+    public class LegacyImportNotificationLogMessage
+    {
+        public int LegacyImportNotificationLogMessageId { get; set; }
+        public int LegacyImportMigrationRunId { get; set; }
+        public string OldNotificationId { get; set; }
+        public LogMessageLevel LogMessageLevel { get; set; }
+        public string Message { get; set; }
+    }
+}
