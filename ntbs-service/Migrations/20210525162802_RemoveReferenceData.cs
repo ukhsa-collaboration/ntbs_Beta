781666fd657edb2ae9b417439070e0b18cd92c8d
LeoCie <leon.cieslik@softwire.com>
2021-05-26 12:27:44 +0100

    NTBS-2356: Add migration and snapshot from removing data

---
+++ ntbs-service/Migrations/20210525162802_RemoveReferenceData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class RemoveReferenceData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // All reference data that was seeded from CSV files has now been removed in favour of seeding from the
+            // geography database.
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
