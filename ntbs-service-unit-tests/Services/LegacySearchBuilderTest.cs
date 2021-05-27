9beb71e184ab3227becc66a7681d1a0ed399181f
Benji Marshall <Benji.Marshall@softwire.com>
2021-05-27 18:14:25 +0100

    Import LTBR Patient ID from new migration field
    LTBR Patient ID is now being tracked separately from group ID, so
    use this new field in the places GroupID was being used as an alias for
    LTBR Patient ID.
    
    When searching for legacy notifications, search using LTBR patient ID,
    as well as ETS and LTBR notification IDs.
    
    When importing a legacy notification, only do it by notification ID.
    This means that if an ETS notification and an LTBR patient ID are the
    same (for different patients), importing by ETS ID will not import that
    LTBR notification, when it wasn't really wanted.

--- ntbs-service-unit-tests/Services/LegacySearchBuilderTest.cs
+++ ntbs-service-unit-tests/Services/LegacySearchBuilderTest.cs
@@ -26,7 +26,7 @@ namespace ntbs_service_unit_tests.Services
         {
             var (sqlQuery, parameters) = ((ILegacySearchBuilder)builder.FilterById("1")).GetResult();
 
-            Assert.Contains("AND (n.PrimaryNotificationId = @id OR n.SecondaryNotificationId = @id OR (n.GroupId = @id AND n.PrimarySource = 'LTBR') OR dmg.NhsNumber = @id)", sqlQuery);
+            Assert.Contains("AND (n.PrimaryNotificationId = @id OR n.SecondaryNotificationId = @id OR n.LtbrPatientId = @id OR dmg.NhsNumber = @id)", sqlQuery);
             Assert.Equal("1", parameters.id);
         }
 
@@ -44,7 +44,7 @@ namespace ntbs_service_unit_tests.Services
         {
             var (sqlQuery, parameters) = ((ILegacySearchBuilder)builder.FilterById("1").FilterByFamilyName("Smith")).GetResult();
 
-            Assert.Contains("AND (n.PrimaryNotificationId = @id OR n.SecondaryNotificationId = @id OR (n.GroupId = @id AND n.PrimarySource = 'LTBR') OR dmg.NhsNumber = @id)", sqlQuery);
+            Assert.Contains("AND (n.PrimaryNotificationId = @id OR n.SecondaryNotificationId = @id OR n.LtbrPatientId = @id OR dmg.NhsNumber = @id)", sqlQuery);
             Assert.Contains("AND (dmg.FamilyName LIKE @familyName)", sqlQuery);
             Assert.Equal("1", parameters.id);
             Assert.Equal("%Smith%", parameters.familyName);
