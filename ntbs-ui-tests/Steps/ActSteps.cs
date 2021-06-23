86feee808171c6b18f8545570935e03a6accd88d
Joshua Cadney <joshua.cadney@softwire.com>
2021-06-23 18:19:14 +0100

    NTBS-2446 Add logging to arrange and act steps also

--- ntbs-ui-tests/Steps/ActSteps.cs
+++ ntbs-ui-tests/Steps/ActSteps.cs
@@ -30,60 +30,84 @@ namespace ntbs_ui_tests.Steps
         [When(@"I navigate to the url of the current notification")]
         public void WhenINavigateToCurrentNotificationUrl()
         {
-            Browser.Navigate().GoToUrl($"{Settings.EnvironmentConfig.RootUri}/Notifications/{TestContext.AddedNotificationIds.Single()}");
-            // Verify that the page has loaded
-            Assert.NotNull(Browser.FindElement(By.ClassName("notification-banner-title-text")));
+            WithErrorLogging(() =>
+            {
+                Browser.Navigate().GoToUrl($"{Settings.EnvironmentConfig.RootUri}/Notifications/{TestContext.AddedNotificationIds.Single()}");
+                // Verify that the page has loaded
+                Assert.NotNull(Browser.FindElement(By.ClassName("notification-banner-title-text")));
+            });
         }
         
         [When(@"I click '(.*)' on the navigation bar")]
         public void ClickOnTheNavigationBar(string label)
         {
-            var pageLink = HtmlElementHelper.FindElementByXpath(Browser, $"//*[@id='navigation-side-menu']/li[contains(.,'{label}')]/a");
-            pageLink.Click();
+            WithErrorLogging(() =>
+            {
+                var pageLink = HtmlElementHelper.FindElementByXpath(Browser, $"//*[@id='navigation-side-menu']/li[contains(.,'{label}')]/a");
+                pageLink.Click();
+            });
         }
 
         [When(@"I log out")]
         public void WhenILogOut()
         {
-            Browser.Navigate().GoToUrl($"{Settings.EnvironmentConfig.RootUri}/Logout");
+            WithErrorLogging(() =>
+            {
+                Browser.Navigate().GoToUrl($"{Settings.EnvironmentConfig.RootUri}/Logout");
+            });
         }
 
         [When(@"I click on the (.*) link")]
         public void WhenIClickOnTheLink(string linkLabel)
         {
-            HtmlElementHelper.FindElementByXpath(Browser, $"//a[contains(.,'{linkLabel}')]").Click();
+            WithErrorLogging(() =>
+            {
+                HtmlElementHelper.FindElementByXpath(Browser, $"//a[contains(.,'{linkLabel}')]").Click();
+            });
         }
 
         [When(@"I go to edit the '(.*)' section")]
         public void WhenIGoToEditTheSection(string section)
         {
-            var sectionId =HtmlElementHelper.GetSectionIdFromSection(section);
-            var link = Browser.FindElement(By.Id($"{sectionId}-title"))
-                .FindElement(By.LinkText("Edit"));
-            link.Click();
+            WithErrorLogging(() =>
+            {
+                var sectionId =HtmlElementHelper.GetSectionIdFromSection(section);
+                var link = Browser.FindElement(By.Id($"{sectionId}-title"))
+                    .FindElement(By.LinkText("Edit"));
+                link.Click();
+            });
         }
 
         [When(@"I take action on the alert with title (.*)")]
         public void WhenITakeActionOnAlert(string title)
         {
-            var alert = HtmlElementHelper.FindElementsByXpath(Browser, "//*[contains(@id, 'alert-')]").Single(a => a.Text.Contains(title));
-            alert.FindElement(By.LinkText("Take action")).Click();
+            WithErrorLogging(() =>
+            {
+                var alert = HtmlElementHelper.FindElementsByXpath(Browser, "//*[contains(@id, 'alert-')]").Single(a => a.Text.Contains(title));
+                alert.FindElement(By.LinkText("Take action")).Click();
+            });
         }
 
         [When(@"I expand manage notification section")]
         public void WhenIExpandNotificationSection()
         {
-            var button = Browser
-                .FindElement(By.Id("manage-notification"))
-                .FindElement(By.TagName("summary"));
-            button.Click();
+            WithErrorLogging(() =>
+            {
+                var button = Browser
+                    .FindElement(By.Id("manage-notification"))
+                    .FindElement(By.TagName("summary"));
+                button.Click();
+            });
         }
 
         [When(@"I expand the '(.*)' section")]
         public void WhenIExpandSection(string sectionId)
         {
-            var element = HtmlElementHelper.FindElementById(Browser, sectionId).FindElement(By.TagName("summary"));
-            element.Click();
+            WithErrorLogging(() =>
+            {
+                var element = HtmlElementHelper.FindElementById(Browser, sectionId).FindElement(By.TagName("summary"));
+                element.Click();
+            });
         }
 
         #endregion
@@ -93,85 +117,109 @@ namespace ntbs_ui_tests.Steps
         [When(@"I uncheck '(.*)'")]
         public void WhenIDeselectCheckbox(string elementId)
         {
-            var element = HtmlElementHelper.FindElementById(Browser, elementId);
-            if (element.Selected)
+            WithErrorLogging(() =>
             {
-                element.Click();
-            }
+                var element = HtmlElementHelper.FindElementById(Browser, elementId);
+                if (element.Selected)
+                {
+                    element.Click();
+                }
+            });
         }
 
         [When(@"I check '(.*)'")]
         [When(@"I select radio value '(.*)'")]
         public void WhenISelectRadioOrCheckbox(string elementId)
         {
-            var element = HtmlElementHelper.FindElementById(Browser, elementId);
-            if (!element.Selected)
+            WithErrorLogging(() =>
             {
-                element.Click();
-            }
+                var element = HtmlElementHelper.FindElementById(Browser, elementId);
+                if (!element.Selected)
+                {
+                    element.Click();
+                }
+            });
         }
 
         [When(@"I click on the '(.*)' button")]
         public void WhenIClickOn(string elementId)
         {
-            var button = HtmlElementHelper.FindElementById(Browser, elementId);
-            button.Click();
+            WithErrorLogging(() =>
+            {
+                var button = HtmlElementHelper.FindElementById(Browser, elementId);
+                button.Click();
+            });
         }
 
         [When(@"I select (.*) from input list '(.*)'")]
         public void WhenISelectFromInputList(string value, string inputListId)
         {
-            var inputList = HtmlElementHelper.FindElementById(Browser, inputListId);
-            inputList.Click();
-            inputList.SendKeys(Keys.Control + "a");
-            inputList.SendKeys(Keys.Delete);
-            inputList.SendKeys(value);
-            HtmlElementHelper.FindElementById(Browser, inputListId+"__option--0").Click();
-            inputList.SendKeys("\t");
+            WithErrorLogging(() =>
+            {
+                var inputList = HtmlElementHelper.FindElementById(Browser, inputListId);
+                inputList.Click();
+                inputList.SendKeys(Keys.Control + "a");
+                inputList.SendKeys(Keys.Delete);
+                inputList.SendKeys(value);
+                HtmlElementHelper.FindElementById(Browser, inputListId+"__option--0").Click();
+                inputList.SendKeys("\t");
+            });
         }
 
         [When(@"I enter (.*) into '(.*)'")]
         public void WhenIEnterValueIntoFieldWithId(string value, string elementId)
         {
-            var element = HtmlElementHelper.FindElementById(Browser, elementId);
-            element.Click();
-            element.SendKeys(Keys.Control + "a");
-            element.SendKeys(Keys.Delete);
-            element.SendKeys(value + "\t");
-            if (!Settings.IsHeadless)
+            WithErrorLogging(() =>
             {
-                Thread.Sleep(1000);
-            }
+                var element = HtmlElementHelper.FindElementById(Browser, elementId);
+                element.Click();
+                element.SendKeys(Keys.Control + "a");
+                element.SendKeys(Keys.Delete);
+                element.SendKeys(value + "\t");
+                if (!Settings.IsHeadless)
+                {
+                    Thread.Sleep(1000);
+                }
+            });
         }
 
         [When(@"I enter '(.*)' into date fields with id '(.*)'")]
         public void WhenIEnterDateIntoFieldWithId(string dateString, string elementId)
         {
-            var date = DateTime.Parse(dateString, new CultureInfo("en-GB").DateTimeFormat);
-            WhenIEnterValueIntoFieldWithId(date.Day.ToString(), elementId + "_Day");
-            WhenIEnterValueIntoFieldWithId(date.Month.ToString(), elementId + "_Month");
-            WhenIEnterValueIntoFieldWithId(date.Year.ToString(), elementId + "_Year");
-            if (!Settings.IsHeadless)
+            WithErrorLogging(() =>
             {
-                Thread.Sleep(1000);
-            }
+                var date = DateTime.Parse(dateString, new CultureInfo("en-GB").DateTimeFormat);
+                WhenIEnterValueIntoFieldWithId(date.Day.ToString(), elementId + "_Day");
+                WhenIEnterValueIntoFieldWithId(date.Month.ToString(), elementId + "_Month");
+                WhenIEnterValueIntoFieldWithId(date.Year.ToString(), elementId + "_Year");
+                if (!Settings.IsHeadless)
+                {
+                    Thread.Sleep(1000);
+                }
+            });
         }
         
         [When(@"I make selection (.*) from (.*) section for '(.*)'")]
         public void WhenISelectValueFromGroupForFieldWithId(string value, string group, string elementId)
         {
-            var selection = HtmlElementHelper.FindElementByXpath(Browser, $"//select[@id='{elementId}']/optgroup[@label='{group}']/option[@value='{value}']");
-            selection.Click();
-            if (!Settings.IsHeadless)
+            WithErrorLogging(() =>
             {
-                Thread.Sleep(1000);
-            }
+                var selection = HtmlElementHelper.FindElementByXpath(Browser, $"//select[@id='{elementId}']/optgroup[@label='{group}']/option[@value='{value}']");
+                selection.Click();
+                if (!Settings.IsHeadless)
+                {
+                    Thread.Sleep(1000);
+                }
+            });
         }
 
         [When(@"I select (.*) for '(.*)'")]
         public void WhenISelectTextFromDropdown(string text, string selectId)
         {
-            new SelectElement(HtmlElementHelper.FindElementById(Browser, selectId)).SelectByText(text);
+            WithErrorLogging(() =>
+            {
+                new SelectElement(HtmlElementHelper.FindElementById(Browser, selectId)).SelectByText(text);
+            });
         }
 
         #endregion
@@ -179,7 +227,25 @@ namespace ntbs_ui_tests.Steps
         [When(@"I wait")]
         public void WhenIWait()
         {
-            Thread.Sleep(3000);
+            WithErrorLogging(() =>
+            {
+                Thread.Sleep(3000);
+            });
+        }
+
+        private void WithErrorLogging(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch
+            {
+                // This is temporary debugging to help determine why certain tests sometimes fail
+                var webElement = (string)((IJavaScriptExecutor)Browser).ExecuteScript("return document.body.innerHTML;");
+                Console.WriteLine(webElement);
+                throw;
+            }
         }
     }
 }
