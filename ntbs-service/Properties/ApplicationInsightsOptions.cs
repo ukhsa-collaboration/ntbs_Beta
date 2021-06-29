042932d6857a4c7e35180014fe205145c823ed45
Benji Marshall <Benji.Marshall@softwire.com>
2021-06-29 11:00:32 +0100

    Add Application Insights Profiler
    Add relevant options to config for this profiler, leaving it disabled
    in load-test because it does not seem to work well with Sentry.

--- ntbs-service/Properties/ApplicationInsightsOptions.cs
+++ ntbs-service/Properties/ApplicationInsightsOptions.cs
@@ -4,6 +4,9 @@
     {
         public bool? Enabled { get; set; }
         public bool? EnableSqlCommandTextInstrumentation { get; set; }
+        public bool? EnableProfiler { get; set; }
+        public float? RandomProfilingOverhead { get; set; }
+        public int? ProfilerDurationSeconds { get; set; }
         public string ConnectionString { get; set; }
     }
 }
