acfe0683a98c2e008880595fa9b1b2f265fd23a1
Benji Marshall <Benji.Marshall@softwire.com>
2021-07-12 17:52:07 +0100

    Use Sqlite DB for DataQualityRepository unit tests
    This a repository using a combination of EF queries and raw SQL, so
    this is best tested against a real SQL database instead of an in-memory
    EF context.

--- ntbs-service-unit-tests/DataAccess/DataQualityRepositoryTests.cs
+++ ntbs-service-unit-tests/DataAccess/DataQualityRepositoryTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using ntbs_service.DataAccess;
 using ntbs_service.Models;
 using ntbs_service.Models.Entities;
@@ -15,63 +14,51 @@ namespace ntbs_service_unit_tests.DataAccess
     // This suite attempts to test EF queries, close to the context. It draws on this article for the setup inspiration
     // (albeit without the extensive multi-provider support):
     // https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/testing-sample
-    public class DataQualityRepositoryTests
+    public class DataQualityRepositoryTests : IClassFixture<DataQualityRepositoryFixture>
     {
+        private readonly NtbsContext _context;
+
         private const string NoAlertsName = "A";
         private const string BirthCountryAlertName = "B";
         private const string ClinicalDatesAlertName = "C";
 
-        public DataQualityRepositoryTests()
+        public DataQualityRepositoryTests(DataQualityRepositoryFixture dataQualityRepositoryFixture)
         {
-            ContextOptions = new DbContextOptionsBuilder<NtbsContext>()
-                .UseInMemoryDatabase(nameof(DataQualityRepositoryTests))
-                .Options;
+            _context = dataQualityRepositoryFixture.Context;
         }
-
-        private DbContextOptions<NtbsContext> ContextOptions { get; }
 
         [Fact]
         public async Task FiltersOutNotificationsWithTheAlertTypeAlreadyPresent()
         {
             // ARRANGE
-            using (var context = new NtbsContext(ContextOptions))
-            {
-                await context.Database.EnsureDeletedAsync();
-                await context.Database.EnsureCreatedAsync();
+            // We will use the birth country alert as a proxy for testing the common path ways of the
+            // data quality repository
+            // All seeded notifications will be eligible for this alert
 
-                // We will use the birth country alert as a proxy for testing the common path ways of the
-                // data quality repository
-                // All seeded notifications will be eligible for this alert
+            var notification1 = NotificationEligibleForCountryOfBirthDqAlert(NoAlertsName);
 
-                var notification1 = NotificationEligibleForCountryOfBirthDqAlert(NoAlertsName);
+            var notification2 = NotificationEligibleForCountryOfBirthDqAlert(BirthCountryAlertName);
+            notification2.Alerts = new List<Alert> { new DataQualityBirthCountryAlert() };
 
-                var notification2 = NotificationEligibleForCountryOfBirthDqAlert(BirthCountryAlertName);
-                notification2.Alerts = new List<Alert> { new DataQualityBirthCountryAlert() };
+            var notification3 = NotificationEligibleForCountryOfBirthDqAlert(ClinicalDatesAlertName);
+            notification3.Alerts = new List<Alert> { new DataQualityClinicalDatesAlert() };
 
-                var notification3 = NotificationEligibleForCountryOfBirthDqAlert(ClinicalDatesAlertName);
-                notification3.Alerts = new List<Alert> { new DataQualityClinicalDatesAlert() };
+            await _context.AddRangeAsync(notification1, notification2, notification3);
+            await _context.SaveChangesAsync();
 
-                await context.AddRangeAsync(notification1, notification2, notification3);
+            var repo = new DataQualityRepository(_context);
 
-                await context.SaveChangesAsync();
-            }
+            // ACT
+            // Get all possible ones
+            var notifications = await repo.GetNotificationsEligibleForDqBirthCountryAlertsAsync(100, 0);
 
-            using (var context = new NtbsContext(ContextOptions))
-            {
-                var repo = new DataQualityRepository(context);
-
-                // ACT
-                // Get all possible ones
-                var notifications = await repo.GetNotificationsEligibleForDqBirthCountryAlertsAsync(100, 0);
-
-                // ASSERT
-                // Out of the 3 eligible notifications, 1 already has the alert
-                Assert.True(notifications.Any(n => n.PatientDetails.FamilyName == NoAlertsName),
-                    "Eligible notification with no other alerts not selected");
-                Assert.True(notifications.Any(n => n.PatientDetails.FamilyName == ClinicalDatesAlertName),
-                    "Eligible notification with only other alerts types not selected");
-                Assert.Equal(2, notifications.Count);
-            }
+            // ASSERT
+            // Out of the 3 eligible notifications, 1 already has the alert
+            Assert.True(notifications.Any(n => n.PatientDetails.FamilyName == NoAlertsName),
+                "Eligible notification with no other alerts not selected");
+            Assert.True(notifications.Any(n => n.PatientDetails.FamilyName == ClinicalDatesAlertName),
+                "Eligible notification with only other alerts types not selected");
+            Assert.Equal(2, notifications.Count);
         }
 
         private static Notification NotificationEligibleForCountryOfBirthDqAlert(string name)
