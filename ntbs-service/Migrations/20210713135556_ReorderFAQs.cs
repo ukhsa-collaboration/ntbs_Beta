c6c23dbe6ad0c4171d4079037793de138b752027
LeoCie <leon.cieslik@softwire.com>
2021-07-13 14:59:39 +0100

    NTBS-2519: Reorder FAQs

---
+++ ntbs-service/Migrations/20210713135556_ReorderFAQs.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ntbs_service.Migrations
+{
+    public partial class ReorderFAQs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "Why must I perform a search before creating a new notification?", "OrderIndex", 2);
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "Why can’t I view the full details of a record?", "OrderIndex", 3);
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "Why do I not have permission to edit a record?", "OrderIndex", 4);
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "What should I do if I want to notify a case of TB for a person who has had TB before?", "OrderIndex", 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "Why must I perform a search before creating a new notification?", "OrderIndex", 1);
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "Why can’t I view the full details of a record?", "OrderIndex", 2);
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "Why do I not have permission to edit a record?", "OrderIndex", 3);
+            migrationBuilder.UpdateData("FrequentlyAskedQuestion", "Question",
+                "What should I do if I want to notify a case of TB for a person who has had TB before?", "OrderIndex", 4);
+        }
+    }
+}
