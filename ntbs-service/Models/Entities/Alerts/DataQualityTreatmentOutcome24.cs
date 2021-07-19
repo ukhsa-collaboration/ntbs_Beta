b8c0eeff24b1b8e05a553805666b85a9bdda874e
Benji Marshall <Benji.Marshall@softwire.com>
2021-07-19 13:42:05 +0100

    Refactor filters for treatment outcome alerts
    Filter by notification status as well as date range for the first pass
    of filtering for treatment outcome DQ alerts. The more complex
    treatment outcome analysis is done in memory for each possibly eligible
    notification, so the more precisely we define that set the less
    notifications the job has to consider each night.

--- ntbs-service/Models/Entities/Alerts/DataQualityTreatmentOutcome24.cs
+++ ntbs-service/Models/Entities/Alerts/DataQualityTreatmentOutcome24.cs
@@ -9,16 +9,16 @@ namespace ntbs_service.Models.Entities.Alerts
     // ReSharper disable once ClassNeverInstantiated.Global - actually instantiated in DataQualityAlertsJob
     public class DataQualityTreatmentOutcome24 : Alert
     {
-        public static readonly Expression<Func<Notification, bool>> NotificationInQualifyingDateRangeExpression =
+        public static readonly Expression<Func<Notification, bool>> NotificationInQualifyingRangeExpression =
             // This corresponds to n.ClinicalDetails.StartingDate but we can't use it directly as LINQ->SQL gives up
-            n => (n.ClinicalDetails.TreatmentStartDate ?? n.ClinicalDetails.DiagnosisDate) < DateTime.Today.AddYears(-2);
+            n => (n.ClinicalDetails.TreatmentStartDate ?? n.ClinicalDetails.DiagnosisDate) < DateTime.Today.AddYears(-2)
+                 && n.NotificationStatus == NotificationStatus.Notified;
 
         public static readonly Func<Notification, bool> NotificationInRangeQualifies =
             n => TreatmentOutcomesHelper.IsTreatmentOutcomeMissingAtXYears(n, 2);
 
         public static readonly Func<Notification, bool> NotificationQualifies =
-            n => n.NotificationStatus == NotificationStatus.Notified
-                 && NotificationInQualifyingDateRangeExpression.Compile()(n)
+            n => NotificationInQualifyingRangeExpression.Compile()(n)
                  && NotificationInRangeQualifies(n);
 
         public override string Action => "Please provide an outcome with appropriate date";
