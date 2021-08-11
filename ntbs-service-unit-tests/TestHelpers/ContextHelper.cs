79e32c8fac3ddf0ffd90ca551004efa9ff16eb6e
LeoCie <leon.cieslik@softwire.com>
2021-08-11 10:57:37 +0100

    NTBS-2600: Add comments

--- ntbs-service-unit-tests/TestHelpers/ContextHelper.cs
+++ ntbs-service-unit-tests/TestHelpers/ContextHelper.cs
@@ -2,6 +2,7 @@
 {
     public static class ContextHelper
     {
+        // This method should be used in the constructor of any unit tests which use an NTBS context
         public static void DisableAudits()
         {
             Audit.Core.Configuration.AuditDisabled = true;
