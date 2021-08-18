72bd2f15b3f18025b651ab92c9db0b0252f96aa5
Joshua Cadney <joshua.cadney@softwire.com>
2021-08-18 08:24:59 +0100

    Bump version number

--- ntbs-service/VersionInfo.cs
+++ ntbs-service/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace ntbs_service
 {
     public static class VersionInfo
     {
-        public const string CurrentVersion = "1.2.0";
-        public static readonly DateTime CurrentVersionDate = new DateTime(2021, 08, 04, 12, 00, 00);
+        public const string CurrentVersion = "1.3.0";
+        public static readonly DateTime CurrentVersionDate = new DateTime(2021, 08, 19, 12, 00, 00);
     }
 }
