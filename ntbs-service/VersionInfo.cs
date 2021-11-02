c8e43e044300300c3ac3bdbaf726291c4b382955
Joshua Cadney <joshua.cadney@softwire.com>
2021-11-02 08:44:45 +0000

    Update release date

--- ntbs-service/VersionInfo.cs
+++ ntbs-service/VersionInfo.cs
@@ -5,6 +5,6 @@ namespace ntbs_service
     public static class VersionInfo
     {
         public const string CurrentVersion = "1.8.0";
-        public static readonly DateTime CurrentVersionDate = new DateTime(2021, 10, 28, 12, 00, 00);
+        public static readonly DateTime CurrentVersionDate = new DateTime(2021, 11, 02, 12, 00, 00);
     }
 }
