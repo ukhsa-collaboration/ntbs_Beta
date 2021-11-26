d9ab58544e69d8672f92d2af0dea46fa32a94e3a
Benji Marshall <Benji.Marshall@softwire.com>
2021-11-26 13:10:26 +0000

    NTBS-2848 Embedded PowerBI dashboard spike
    Add a new page to demo an embedded PowerBI dashboard, with a little CSS
    for the dashboard iframe.
    
    Configure the dashboard URL using Kubernetes config. This follows the
    existing pattern for external links like the reporting link.

---
+++ ntbs-service/Pages/PowerBiDashboard.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using ntbs_service.Properties;
+
+namespace ntbs_service.Pages
+{
+    public class PowerBiDashboard : PageModel
+    {
+        public string DashboardUrl { get; }
+
+        public PowerBiDashboard(IConfiguration configuration)
+        {
+            var links = new ExternalLinks();
+            configuration.GetSection(Constants.ExternalLinks).Bind(links);
+
+            DashboardUrl = links.EmbeddedPowerBiDashboard;
+        }
+    }
+}
