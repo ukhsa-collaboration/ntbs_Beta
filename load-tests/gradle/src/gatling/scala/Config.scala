84d3ed811e89666255614c42397c0a83da05f152
Joshua Cadney <joshua.cadney@softwire.com>
2022-01-21 09:21:44 +0000

    NTBS-2919 Make url under test configurable

--- load-tests/gradle/src/gatling/scala/Config.scala
+++ load-tests/gradle/src/gatling/scala/Config.scala
@@ -2,7 +2,10 @@ import java.net.HttpCookie;
 import scala.collection.JavaConverters._
 
 object Config {
-    val urlUnderTest = "https://ntbs-load-test.e32846b1ddf0432eb63f.northeurope.aksapp.io"
+    val urlUnderTest = sys.env.getOrElse(
+        "URL_UNDER_TEST",
+        "https://ntbs-load-test.e32846b1ddf0432eb63f.northeurope.aksapp.io"
+    )
     val lengthOfTestInMinutes = sys.env.getOrElse("LOAD_TEST_DURATION_IN_MINUTES", "1").toInt
     val firstNotification = sys.env.getOrElse("FIRST_NOTIFICATION", "300001").toInt
     val numberOfNotifications = sys.env.getOrElse("NUMBER_OF_NOTIFICATIONS", "10000").toInt
