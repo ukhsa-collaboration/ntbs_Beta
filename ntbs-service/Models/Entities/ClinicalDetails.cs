c7c8bd826cf05d83e26f3befd4b84beda2a90e15
Jordan <jordan.d.carlton@gmail.com>
2022-02-03 10:54:58 +0000

    NTBS-3028 add referral to tb service date to edit screen

--- ntbs-service/Models/Entities/ClinicalDetails.cs
+++ ntbs-service/Models/Entities/ClinicalDetails.cs
@@ -26,6 +26,15 @@ namespace ntbs_service.Models.Entities
         [ValidClinicalDate]
         [AssertThat(@"AfterDob(FirstPresentationDate)", ErrorMessage = ValidationMessages.DateShouldBeLaterThanDob)]
         public DateTime? FirstPresentationDate { get; set; }
+        
+        [Display(Name = "Referral to TB service received")]
+        [ValidClinicalDate]
+        [AssertThat(@"AfterDob(FirstPresentationDate)", ErrorMessage = ValidationMessages.DateShouldBeLaterThanDob)]
+        // Assert that BeforeDob(TBServicePresentationDate)
+        // Assert that BeforeDob(DiagnosisDate)
+        // Assert that BeforeDob(Today)
+        // Assert that AfterDob(01/01/2010)
+        public DateTime? TBServiceReferralDate { get; set; }
 
         [Display(Name = "Presentation to TB service")]
         [ValidClinicalDate]
