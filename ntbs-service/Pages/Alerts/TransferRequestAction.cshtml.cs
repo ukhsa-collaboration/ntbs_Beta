95e6aab4a38cf4daa21e1860a9e11c8883ae8029
Jordan <jordan.d.carlton@gmail.com>
2022-02-10 15:01:47 +0000

    NTBS-3034 refactor

--- ntbs-service/Pages/Alerts/TransferRequestAction.cshtml.cs
+++ ntbs-service/Pages/Alerts/TransferRequestAction.cshtml.cs
@@ -70,13 +70,8 @@ namespace ntbs_service.Pages.Alerts
             TransferAlert = await _alertRepository.GetOpenTransferAlertByNotificationId(NotificationId);
             await AuthorizeAndSetBannerAsync();
 
-            if (TransferAlert == null)
-            {
-                return RedirectToPage("/Notifications/Overview", new { NotificationId });
-            }
-            
             // Check edit permission of user and redirect if not allowed
-            if (!await _authorizationService.IsUserAuthorizedToManageAlert(User, TransferAlert))
+            if (TransferAlert is null || !await _authorizationService.IsUserAuthorizedToManageAlert(User, TransferAlert))
             {
                 return RedirectToPage("/Notifications/Overview", new { NotificationId });
             }
