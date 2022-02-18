1a96c75980f28857e0c5aab67d2d98d67d11cc7d
Jordan <jordan.d.carlton@gmail.com>
2022-02-18 16:26:05 +0000

    NTBS-2714 move wrapper to models folder

---
+++ ntbs-service/Models/ServiceDirectoryItemWrapper.cs
@@ -0,0 +1,40 @@
+﻿using ntbs_service.Models.Entities;
+using ntbs_service.Models.ReferenceEntities;
+
+namespace ntbs_service.Models;
+
+public class ServiceDirectoryItemWrapper
+{
+    public User User { get; }
+    public PHEC Region { get; }
+    public TBService TBService { get; }
+    public Hospital Hospital { get; }
+
+    public ServiceDirectoryItemWrapper(User user = null, PHEC region = null, TBService tbService = null, Hospital hospital = null)
+    {
+        this.User = user;
+        this.Region = region;
+        this.TBService = tbService;
+        this.Hospital = hospital;
+    }
+
+    public bool IsUser()
+    {
+        return this.User is not null;
+    }
+
+    public bool IsRegion()
+    {
+        return this.Region is not null;
+    }
+
+    public bool IsTBService()
+    {
+        return this.TBService is not null;
+    }
+
+    public bool IsHospital()
+    {
+        return this.Hospital is not null;
+    }
+}
--- ntbs-service/Services/ServiceDirectoryService.cs
+++ ntbs-service/Services/ServiceDirectoryService.cs
@@ -6,42 +6,6 @@ using ntbs_service.Models.ReferenceEntities;
 
 namespace ntbs_service.Services
 {
-    public class ServiceDirectoryItemWrapper
-    {
-        public User User { get; }
-        public PHEC Region { get; }
-        public TBService TBService { get; }
-        public Hospital Hospital { get; }
-
-        public ServiceDirectoryItemWrapper(User user = null, PHEC region = null, TBService tbService = null, Hospital hospital = null)
-        {
-            this.User = user;
-            this.Region = region;
-            this.TBService = tbService;
-            this.Hospital = hospital;
-        }
-
-        public bool IsUser()
-        {
-            return this.User is not null;
-        }
-
-        public bool IsRegion()
-        {
-            return this.Region is not null;
-        }
-
-        public bool IsTBService()
-        {
-            return this.TBService is not null;
-        }
-
-        public bool IsHospital()
-        {
-            return this.Hospital is not null;
-        }
-    }
-
     public interface IServiceDirectoryService
     {
         (IList<ServiceDirectoryItemWrapper>, int) GetPaginatedItems(
