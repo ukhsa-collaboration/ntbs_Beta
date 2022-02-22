60cec1b3801a92526d05e4f4995790bd8e4648f4
Jordan <jordan.d.carlton@gmail.com>
2022-02-22 12:42:54 +0000

    NTBS-2714 refactor to SearchStringHelper and add tests

---
+++ ntbs-service/Helpers/SearchStringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ntbs_service.Helpers
+{
+    
+    public static class SearchStringHelper
+    {
+        public static List<string> GetSearchKeywords(string searchKeyword)
+        {
+            return searchKeyword
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.ToLower())
+                .ToList();
+        }
+    }
+}
+
