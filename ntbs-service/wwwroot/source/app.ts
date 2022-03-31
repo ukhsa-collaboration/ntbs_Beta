bc0bdeabb8f34f2e5789bf02d560c3c598445f74
Jordan <jordan.d.carlton@gmail.com>
2022-03-31 10:27:10 +0100

    NTBS-3113 add garbage sentry dsn

--- ntbs-service/wwwroot/source/app.ts
+++ ntbs-service/wwwroot/source/app.ts
@@ -55,7 +55,7 @@ require("./Polyfills/ArrayFromPolyfill");
 
 if (config.env === "production") {
     Sentry.init({
-        dsn: 'https://83b245064a684fa7ac86658bf38d2ad3@sentry.io/2862017', // identifies the sentry project
+        dsn: 'https://83b245064a684c86658bf38d2ad3@sentry.io/28620', // identifies the sentry project
         integrations: [new SentryIntegrations.Vue({Vue, attachProps: true, logErrors: true})],
     });
 }
