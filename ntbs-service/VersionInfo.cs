ae55a40a9a4c7b6dcd12ee9abe806c00d9522ef7
adilmirzaphe <54138029+adilmirzaphe@users.noreply.github.com>
2022-11-18 08:33:20 +0000

    Update VersionInfo.cs

--- ntbs-service/VersionInfo.cs
+++ ntbs-service/VersionInfo.cs
@@ -4,7 +4,7 @@ namespace ntbs_service
 {
     public static class VersionInfo
     {
-        public const string CurrentVersion = "1.17.0";
-        public static readonly DateTime CurrentVersionDate = new DateTime(2022, 06, 29, 12, 00, 00);
+        public const string CurrentVersion = "1.18.0";
+        public static readonly DateTime CurrentVersionDate = new DateTime(2022, 11, 18, 12, 00, 00);
     }
 }
